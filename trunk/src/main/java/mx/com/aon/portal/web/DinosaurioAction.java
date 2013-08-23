d52a2efd1e108c6f19030610e9bb5620f6ddd21f
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2013-08-23 23:40:25 +0000

    Se quitó el estilo position:relative de div.wrapper debido a que ocasiona un div flotante

--- trunk/src/main/java/mx/com/aon/portal/web/DinosaurioAction.java
+++ trunk/src/main/java/mx/com/aon/portal/web/DinosaurioAction.java
@@ -4,8 +4,11 @@
  */
 package mx.com.aon.portal.web;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import mx.com.aon.core.web.PrincipalCoreAction;
+import mx.com.aon.portal.web.model.CotizacionSaludVO;
+import mx.com.aon.portal2.web.GenericVO;
 import org.apache.struts2.interceptor.CookiesAware;
 
 /**
@@ -17,11 +20,23 @@ public class DinosaurioAction extends PrincipalCoreAction implements CookiesAwar
     private Map<String, String> cookiesMap;
     private Dinosaurio dino;
     private String llave;
+    private CotizacionSaludVO cotizacion;
     
     public String jsoo()
     {
-        System.out.println("### dino "+(dino!=null?dino:"null"));
-        System.out.println("### llave "+(llave!=null?llave:"null"));
+        cotizacion=new CotizacionSaludVO();
+        cotizacion.setId(                                       69l);
+        cotizacion.setCiudad(                                   new GenericVO("04","lol"));
+        cotizacion.setDeducible(                                new BigDecimal("69"));
+        cotizacion.setCopago(                                   new GenericVO("10000","lol"));
+        cotizacion.setSumaSegurada(                             new GenericVO("14000","lol"));
+        cotizacion.setCirculoHospitalario(                      new GenericVO("3","lol"));
+        cotizacion.setCoberturaVacunas(                         new GenericVO("S","lol"));
+        cotizacion.setCoberturaPrevencionEnfermedadesAdultos(   new GenericVO("S","lol"));
+        cotizacion.setMaternidad(                               new GenericVO("S","lol"));
+        cotizacion.setSumaAseguradaMaternidad(                  new GenericVO("100000","lol"));
+        cotizacion.setBaseTabuladorReembolso(                   new GenericVO("35000","lol"));
+        cotizacion.setCostoEmergenciaExtranjero(                new GenericVO("S","lol"));
         return SUCCESS;
     }
     
@@ -47,6 +62,14 @@ public class DinosaurioAction extends PrincipalCoreAction implements CookiesAwar
 
     public void setLlave(String llave) {
         this.llave = llave;
+    }
+
+    public CotizacionSaludVO getCotizacion() {
+        return cotizacion;
+    }
+
+    public void setCotizacion(CotizacionSaludVO cotizacion) {
+        this.cotizacion = cotizacion;
     }
     
 }
