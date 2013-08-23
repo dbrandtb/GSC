2a1ad7c5fe5b4e63d197dd46506d3150f83f9408
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2013-08-23 23:28:53 +0000

    Se quitó el estilo position:relative de div.wrapper debido a que ocasiona un div flotante

--- trunk/src/main/webapp/resources/extjs4/base_extjs4.js
+++ trunk/src/main/webapp/resources/extjs4/base_extjs4.js
@@ -1,20 +1,75 @@
 ////////////////////////////////////////////////////////////////////
 // ESTE DOCUMENTO ES LA BASE DE TODOS LOS JSP QUE TENGAN EXT JS 4 //
 ////////////////////////////////////////////////////////////////////
-Ext.onReady(function(){
-    
-    ///////////////////////////////////////////////////////////////
-    // SE DEFINE EL MODELO "Generic" QUE ALIMENTA COMBOS Y OTROS //
-    ///////////////////////////////////////////////////////////////
-    Ext.define('Generic',
-    {
-        extend   : 'Ext.data.Model',
-        fields   :
-        [
-            {name:'key',    type: 'string'},
-            {name:'value',  type: 'string'}
-        ]
-    });
-    ///////////////////////////////////////////////////////////////
-    
+
+////////////////////////////
+////// INICIO MODELOS //////
+////////////////////////////
+Ext.define('Generic',
+{
+    extend:'Ext.data.Model',
+    fields:
+    [
+        {name:'key',    type: 'string'},
+        {name:'value',  type: 'string'}
+    ]
 });
+
+Ext.define('GeCiudad',                                  {extend:'Generic'});
+Ext.define('GeSexo',                                    {extend:'Generic'});
+Ext.define('GeRol',                                     {extend:'Generic'});
+Ext.define('GeCopago',                                  {extend:'Generic'});
+Ext.define('GeSumaAsegurada',                           {extend:'Generic'});
+Ext.define('GeCirculoHospitalario',                     {extend:'Generic'});
+Ext.define('GeCoberturaVacunas',                        {extend:'Generic'});
+Ext.define('GeCoberturaPrevencionEnfermedadesAdultos',  {extend:'Generic'});
+Ext.define('GeMaternidad',                              {extend:'Generic'});
+Ext.define('GeSumaAseguradaMaternidad',                 {extend:'Generic'});
+Ext.define('GeBaseTabuladorReembolso',                  {extend:'Generic'});
+Ext.define('GeCostoEmergenciaExtranjero',               {extend:'Generic'});
+
+Ext.define('IncisoSalud',
+{
+    extend:'Ext.data.Model',
+    fields:
+    [
+        {name:'id',                 type:'numeric'},
+        {name:'rol',                type:'GeRol'},
+        {name:'fechaNacimiento',    type:'date'},
+        {name:'sexo',               type:'GeSexo'},
+        {name:'nombre',             type:'string'},
+        {name:'segundoNombre',      type:'string'},
+        {name:'apellidoPaterno',    type:'string'},
+        {name:'apellidoMaterno',    type:'string'}
+    ]
+});
+
+Ext.define('CotizacionSalud',
+{
+    extend:'Ext.data.Model',
+    fields:
+    [
+        {name:'id',                                     type:'numeric'},
+        {name:'ciudad',                                 type:'GeCiudad'},
+        {name:'deducible',                              type:'numeric'},
+        {name:'copago',                                 type:'GeCopago'},
+        {name:'sumaSegurada',                           type:'GeSumaAsegurada'},
+        {name:'circuloHospitalario',                    type:'GeCirculoHospitalario'},
+        {name:'coberturaVacunas',                       type:'GeCoberturaVacunas'},
+        {name:'coberturaPrevencionEnfermedadesAdultos', type:'GeCoberturaPrevencionEnfermedadesAdultos'},
+        {name:'maternidad',                             type:'GeMaternidad'},
+        {name:'sumaAseguradaMaternidad',                type:'GeSumaAseguradaMaternidad'},
+        {name:'baseTabuladorReembolso',                 type:'GeBaseTabuladorReembolso'},
+        {name:'costoEmergenciaExtranjero',              type:'GeCostoEmergenciaExtranjero'}
+    ],
+    hasMany:
+    [{
+        name:           'incisos',
+        model:          'IncisoSalud',
+        foreignKey:     'incisos',
+        associationKey: 'incisos'
+    }]
+});
+/////////////////////////
+////// FIN MODELOS //////
+/////////////////////////
