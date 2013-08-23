0d668ca69f4d54da2be354fca7509d3d2771996e
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2013-08-23 23:30:20 +0000

    Se quitó el estilo position:relative de div.wrapper debido a que ocasiona un div flotante

--- trunk/src/main/java/mx/com/aon/portal2/web/JsonGenericList.java
+++ trunk/src/main/java/mx/com/aon/portal2/web/JsonGenericList.java
@@ -7,6 +7,7 @@ package mx.com.aon.portal2.web;
 import java.util.ArrayList;
 import java.util.List;
 import mx.com.aon.core.web.PrincipalCoreAction;
+import mx.com.aon.portal.service.CombosManager2;
 
 /**
  *
@@ -17,109 +18,29 @@ public class JsonGenericList extends PrincipalCoreAction
 // ESTA CLASE UTILIZA UNA LISTA GENERICA PARA REGRESAR LISTAS DE COMBO Y OTROS //
 /////////////////////////////////////////////////////////////////////////////////
 {
+    private List<GenericVO> lista;
+    private CombosManager2 combosManager2;
+    private String cdatribu;
+    private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(JsonGenericList.class);
     
-    private List<GenericVO> lista=new ArrayList<GenericVO>(0);
-    
-    public String obtenCiudades()
+    public String obtenCatalogo()
     {
-        lista.add(new GenericVO("1","Guadalajara, Jalisco"));
-        lista.add(new GenericVO("2","Hermosillo, Sonora"));
-        lista.add(new GenericVO("3","Ciudad Juárez, Chihuahua"));
-        lista.add(new GenericVO("4","Matamoros, Tamaulipas"));
-        lista.add(new GenericVO("5","Torreón, Choahuila"));
-        return SUCCESS;
-    }
-    
-    public String obtenCopagos()
-    {
-        lista.add(new GenericVO("1","10%"));
-        lista.add(new GenericVO("2","15%"));
-        lista.add(new GenericVO("3","20%"));
-        lista.add(new GenericVO("4","25%"));
-        return SUCCESS;
-    }
-    
-    public String obtenSumasAseguradas()
-    {
-        lista.add(new GenericVO("1","$1,500,000"));
-        lista.add(new GenericVO("2","$2,000,000"));
-        lista.add(new GenericVO("3","$3,000,000"));
-        lista.add(new GenericVO("4","$5,000,000"));
-        return SUCCESS;
-    }
-    
-    public String obtenCirculosHospitalarios()
-    {
-        lista.add(new GenericVO("1","Alto"));
-        lista.add(new GenericVO("2","Medio"));
-        lista.add(new GenericVO("3","Bajo"));
-        return SUCCESS;
-    }
-    
-    public String obtenCoberturasVacunas()
-    {
-        lista.add(new GenericVO("1","Si"));
-        lista.add(new GenericVO("2","No"));
-        return SUCCESS;
-    }
-    
-    public String obtenCoberturasPrevencionEnfermedadesAdultos()
-    {
-        lista.add(new GenericVO("1","Si"));
-        lista.add(new GenericVO("2","No"));
-        return SUCCESS;
-    }
-    
-    public String obtenMaternidad()
-    {
-        lista.add(new GenericVO("1","Obligatoria"));
-        lista.add(new GenericVO("2","Ayuda Suavizada"));
-        lista.add(new GenericVO("3","Ambos"));
-        return SUCCESS;
-    }
-    
-    public String obtenSumaAseguradaMaternidad()
-    {
-        lista.add(new GenericVO("1","7.5 SMMG"));
-        lista.add(new GenericVO("2","10 SMMG"));
-        lista.add(new GenericVO("3","15 SMMG"));
-        return SUCCESS;
-    }
-    
-    public String obtenBaseTabuladorReembolso()
-    {
-        lista.add(new GenericVO("1","21,000"));
-        lista.add(new GenericVO("2","25,000"));
-        lista.add(new GenericVO("3","30,000"));
-        lista.add(new GenericVO("4","35,000"));
-        lista.add(new GenericVO("5","40,000"));
-        lista.add(new GenericVO("6","45,000"));
-        lista.add(new GenericVO("7","50,000"));
-        return SUCCESS;
-    }
-    
-    public String obtenCostoEmergenciaExtranjero()
-    {
-        lista.add(new GenericVO("1","ZI Individual"));
-        lista.add(new GenericVO("2","ZF Individual"));
-        lista.add(new GenericVO("3","Excluida"));
-        return SUCCESS;
-    }
-    
-    public String obtenGeneros()
-    {
-        lista.add(new GenericVO("1","Hombre"));
-        lista.add(new GenericVO("2","Mujer"));
-        return SUCCESS;
-    }
-    
-    public String obtenRoles()
-    {
-        lista.add(new GenericVO("1","Titular"));
-        lista.add(new GenericVO("2","Hermano(a)"));
-        lista.add(new GenericVO("3","Hijo(a)"));
-        lista.add(new GenericVO("4","Cónyugue"));
-        lista.add(new GenericVO("5","Padre o madre"));
+        try
+        {
+            if(cdatribu!=null&&cdatribu.equals("roles"))//solo para el caso en que obtenga roles (Titular, cónyugue, hijo...)
+            {
+                lista=combosManager2.obtenCatalogoRoles("2");
+            }
+            else//para los demas catalogos
+            {
+                lista=combosManager2.obtenCatalogoSaludVital("", "SL", cdatribu);
+            }
+        }
+        catch(Exception ex)
+        {
+            lista=new ArrayList<GenericVO>(0);
+            log.error("No se pudo obtener el catalogo "+cdatribu, ex);
+        }
         return SUCCESS;
     }
 
@@ -129,6 +50,18 @@ public class JsonGenericList extends PrincipalCoreAction
 
     public void setLista(List<GenericVO> lista) {
         this.lista = lista;
+    }
+    
+    public void setCombosManager2(CombosManager2 combosManager2) {
+        this.combosManager2 = combosManager2;
+    }
+
+    public String getCdatribu() {
+        return cdatribu;
+    }
+
+    public void setCdatribu(String cdatribu) {
+        this.cdatribu = cdatribu;
     }
 }
 /////////////////////////////////////////////////////////////////////////////////
