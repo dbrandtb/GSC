63568d39c4f7972a2dcda40bfcbfe8212ac9f56d
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2013-08-29 23:01:54 +0000

    Se eliminaron las pruebas unitarias de la aplicacion anterior.

--- trunk/src/test/java/mx/com/aon/portal/web/tests/CatalogosCompuestosTest.java
+++
@@ -1,8 +0,0 @@
-package mx.com.aon.portal.web.tests;
-
-import mx.com.aon.portal.service.ConfiguradorCatalogosCompuestosManager;
-
-public class CatalogosCompuestosTest extends AbstractTestCases {
-	protected ConfiguradorCatalogosCompuestosManager configuradorCatalogosCompuestosManager;
-
-}
