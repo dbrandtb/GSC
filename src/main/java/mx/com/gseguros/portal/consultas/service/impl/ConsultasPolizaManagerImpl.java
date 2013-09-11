41d444160a981f3c2053d64fab3cfe1a40238df9
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-09-11 18:50:32 +0000

    Se agrego el servicio de Consulta Datos Tarifa para la Poliza

--- src/main/java/mx/com/gseguros/portal/consultas/service/impl/ConsultasPolizaManagerImpl.java
+++ src/main/java/mx/com/gseguros/portal/consultas/service/impl/ConsultasPolizaManagerImpl.java
@@ -5,6 +5,7 @@ import static mx.com.gseguros.portal.consultas.dao.ConsultasPolizaDAO.OBTIENE_DA
 import static mx.com.gseguros.portal.consultas.dao.ConsultasPolizaDAO.OBTIENE_DATOS_SITUACION;
 import static mx.com.gseguros.portal.consultas.dao.ConsultasPolizaDAO.OBTIENE_DATOS_COBERTURAS;
 import static mx.com.gseguros.portal.consultas.dao.ConsultasPolizaDAO.OBTIENE_POLIZAS_ASEGURADO;
+import static mx.com.gseguros.portal.consultas.dao.ConsultasPolizaDAO.OBTIENE_DATOS_TARIFA;
 
 import java.util.HashMap;
 
@@ -13,81 +14,104 @@ import mx.com.aon.portal.service.impl.AbstractManagerJdbcTemplateInvoke;
 import mx.com.aon.portal.util.WrapperResultados;
 import mx.com.gseguros.portal.consultas.service.ConsultasPolizaManager;
 
-public class ConsultasPolizaManagerImpl extends AbstractManagerJdbcTemplateInvoke implements ConsultasPolizaManager {
+public class ConsultasPolizaManagerImpl extends
+		AbstractManagerJdbcTemplateInvoke implements ConsultasPolizaManager {
 
 	public WrapperResultados consultaPoliza(String cdunieco, String cdramo,
 			String estado, String nmpoliza, String idper, String nmclient)
 			throws ApplicationException {
-		
-		HashMap<String,Object> params = new HashMap<String,Object>();  
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
 		params.put("pv_cdunieco_i", cdunieco);
 		params.put("pv_cdramo_i", cdramo);
 		params.put("pv_estado_i", estado);
 		params.put("pv_nmpoliza_i", nmpoliza);
 		params.put("pv_cdideper_i", idper);
 		params.put("pv_nmclient_i", nmclient);
-		
-		WrapperResultados result = this.returnBackBoneInvoke(params, OBTIENE_DATOS_POLIZA);
-		
+
+		WrapperResultados result = this.returnBackBoneInvoke(params,
+				OBTIENE_DATOS_POLIZA);
+
 		return result;
 	}
-	public WrapperResultados consultaSuplemento(String cdunieco, String cdramo, String estado, String nmpoliza)
-					throws ApplicationException {
-		
-		HashMap<String,Object> params = new HashMap<String,Object>();  
+
+	public WrapperResultados consultaSuplemento(String cdunieco, String cdramo,
+			String estado, String nmpoliza) throws ApplicationException {
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
 		params.put("pv_cdunieco_i", cdunieco);
 		params.put("pv_cdramo_i", cdramo);
 		params.put("pv_estado_i", estado);
 		params.put("pv_nmpoliza_i", nmpoliza);
-		
-		WrapperResultados result = this.returnBackBoneInvoke(params, OBTIENE_DATOS_SUPLEMENTO);
-		
+
+		WrapperResultados result = this.returnBackBoneInvoke(params,
+				OBTIENE_DATOS_SUPLEMENTO);
+
 		return result;
 	}
-	public WrapperResultados consultaSituacion(String cdunieco, String cdramo, String estado, String nmpoliza, String suplemento, String nmsituac)
-					throws ApplicationException {
-		
-		HashMap<String,Object> params = new HashMap<String,Object>();  
-		params.put("pv_cdunieco_i", cdunieco);
-		params.put("pv_cdramo_i", cdramo);
-		params.put("pv_estado_i", estado);
-		params.put("pv_nmpoliza_i", nmpoliza);
-		params.put("pv_nmsuplem_i", suplemento);
-		params.put("pv_nmsituac_i", nmsituac);
-		
-		WrapperResultados result = this.returnBackBoneInvoke(params, OBTIENE_DATOS_SITUACION);
-		
-		return result;
-	}
-	public WrapperResultados consultaCoberturas(String cdunieco, String cdramo, String estado, String nmpoliza, String suplemento, String nmsituac)
-					throws ApplicationException {
-		
-		HashMap<String,Object> params = new HashMap<String,Object>();  
+
+	public WrapperResultados consultaSituacion(String cdunieco, String cdramo,
+			String estado, String nmpoliza, String suplemento, String nmsituac)
+			throws ApplicationException {
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
 		params.put("pv_cdunieco_i", cdunieco);
 		params.put("pv_cdramo_i", cdramo);
 		params.put("pv_estado_i", estado);
 		params.put("pv_nmpoliza_i", nmpoliza);
 		params.put("pv_nmsuplem_i", suplemento);
 		params.put("pv_nmsituac_i", nmsituac);
-		
-		WrapperResultados result = this.returnBackBoneInvoke(params, OBTIENE_DATOS_COBERTURAS);
-		
+
+		WrapperResultados result = this.returnBackBoneInvoke(params,
+				OBTIENE_DATOS_SITUACION);
+
+		return result;
+	}
+
+	public WrapperResultados consultaCoberturas(String cdunieco, String cdramo,
+			String estado, String nmpoliza, String suplemento, String nmsituac)
+			throws ApplicationException {
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("pv_cdunieco_i", cdunieco);
+		params.put("pv_cdramo_i", cdramo);
+		params.put("pv_estado_i", estado);
+		params.put("pv_nmpoliza_i", nmpoliza);
+		params.put("pv_nmsuplem_i", suplemento);
+		params.put("pv_nmsituac_i", nmsituac);
+
+		WrapperResultados result = this.returnBackBoneInvoke(params,
+				OBTIENE_DATOS_COBERTURAS);
+
 		return result;
 	}
 
 	public WrapperResultados obtienePolizasAsegurado(String rfc)
 			throws ApplicationException {
-		
-		HashMap<String,Object> params = new HashMap<String,Object>();  
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
 		params.put("pv_cdrfc", rfc);
-		
-		WrapperResultados result = this.returnBackBoneInvoke(params, OBTIENE_POLIZAS_ASEGURADO);
-		
+
+		WrapperResultados result = this.returnBackBoneInvoke(params,
+				OBTIENE_POLIZAS_ASEGURADO);
+
 		return result;
 	}
-	
-	
-	
-	
-	
+
+	public WrapperResultados consultaDatosTarifa(String cdunieco,
+			String cdramo, String estado, String nmpoliza, String suplemento) throws ApplicationException {
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("pv_cdunieco_i", cdunieco);
+		params.put("pv_cdramo_i", cdramo);
+		params.put("pv_estado_i", estado);
+		params.put("pv_nmpoliza_i", nmpoliza);
+		params.put("pv_nmsuplem_i", suplemento);
+
+		WrapperResultados result = this.returnBackBoneInvoke(params,
+				OBTIENE_DATOS_TARIFA);
+
+		return result;
+	}
+
 }
