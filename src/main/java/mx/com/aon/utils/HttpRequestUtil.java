3931c5bb276fc31293667ce600f192a1a8a84f0a
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-09-22 06:55:29 +0000

    Se agrega metodo para generar reporte

--- src/main/java/mx/com/aon/utils/HttpRequestUtil.java
+++ src/main/java/mx/com/aon/utils/HttpRequestUtil.java
@@ -1,7 +1,12 @@
 package mx.com.aon.utils;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -16,6 +21,55 @@ public class HttpRequestUtil {
 	public static final String GET = "GET";
 	public static final String POST = "POST";
 	public static final int CODIGO_RESPUESTA_OK = 200;
+	
+	public static void generaReporte(String urlInicio, String rutaDestino) throws Exception {
+		String inputLine;
+		URL obj = new URL(urlInicio);
+
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod(GET);
+
+		int responseCode = con.getResponseCode();
+		if (responseCode != CODIGO_RESPUESTA_OK) {
+			logger.debug("Codigo de Respuesta : " + responseCode);
+			throw new Exception("Codigo de respuesta erroneo: " + responseCode);
+		}
+		InputStream inputStream = con.getInputStream();
+		OutputStream outputStream = null;
+
+		try {
+			// write the inputStream to a FileOutputStream
+			outputStream = new FileOutputStream(new File(rutaDestino));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			while ((read = inputStream.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+			}
+			logger.debug("Archivo creado");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (outputStream != null) {
+				try {
+					// outputStream.flush();
+					outputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+		
+	}
 
 	public static void creaPeticionGet(String url) throws Exception {
 		String inputLine;
