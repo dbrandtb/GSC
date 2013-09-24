bc6af75a162bd2e36df6614654a0c67a946a0cea
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-09-24 18:29:31 +0000

    Se eliminaron las variables de LDAP

--- src/main/java/mx/com/gseguros/utils/Constantes.java
+++ src/main/java/mx/com/gseguros/utils/Constantes.java
@@ -2,11 +2,5 @@ package mx.com.gseguros.utils;
 
 public interface Constantes {
 	
-	public static final String USER = "USUARIO";
-	public static final String URLLDAP ="ldap://192.168.1.190:389";
-	public static final String ContextoLDAP = "com.sun.jndi.ldap.LdapCtxFactory";
-	public static final String TipoAuthLDAP = "simple";
-	public static final String UsuarioLDAP = "cn=orcladmin";
-	public static final String PasswordLDAP = "config98";
-	public static final String SearchBaseLDAP = "cn=Users,dc=biosnettcs,dc=com";
+	public static final String USER = "USUARIO";	
 }
