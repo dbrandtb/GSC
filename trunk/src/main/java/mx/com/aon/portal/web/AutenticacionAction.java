d04731b451128fbffcdf4c6e636a0914c9a8decb
albertoGC <albertoGC@68539698-d818-11e6-ad83-4fbd610fe048>
2013-09-25 00:08:53 +0000

    Se actualizaron los mensajes que daría como respuesta al usuario

--- trunk/src/main/java/mx/com/aon/portal/web/AutenticacionAction.java
+++ trunk/src/main/java/mx/com/aon/portal/web/AutenticacionAction.java
@@ -64,15 +64,15 @@ public class AutenticacionAction extends ActionSupport implements SessionAware {
 		try {
 			boolean existeUsuario = validaUsuarioLdap(user, password);
 			if (!existeUsuario) {
-				errorMessage = "EL usuario no existe o la clave es incorrecta";
+				errorMessage = "El usuario no existe o la clave es incorrecta";
 			} else {
 				success = creaSesionDeUsuario(user);
 			}
 			return SUCCESS;
 
 		} catch (Exception ex) {
-			logger.error("Error en el proceso Interno", ex);
-			errorMessage = "Error en el proceso Interno";
+			logger.error(ex.getMessage(), ex);
+			errorMessage = ex.getMessage();
 			return SUCCESS;
 		}
 	}
@@ -100,7 +100,7 @@ public class AutenticacionAction extends ActionSupport implements SessionAware {
 
 		if (listaRolCliente == null || listaRolCliente.isEmpty()) {
 			session.clear();
-			errorMessage = "Usted no posee un Rol Asociado por favor contacte al Administrador!";
+			errorMessage = "Usted no posee un rol asociado, por favor contacte al administrador";
 		} else {
 			exito = true;
 			errorMessage = "EXITO";
@@ -164,8 +164,8 @@ public class AutenticacionAction extends ActionSupport implements SessionAware {
 		this.session = session;
 	}
 
-	public boolean validaUsuarioLdap(String user, String password) {
-		
+	public boolean validaUsuarioLdap(String user, String password) throws Exception {
+		DirContext ctx;
 		logger.debug("URLLDAP=" +        this.getText("validacion.ldap.URLLDAP"));
 		logger.debug("ContextoLDAP=" +   this.getText("validacion.ldap.ContextoLDAP"));
 		logger.debug("TipoAuthLDAP=" +   this.getText("validacion.ldap.TipoAuthLDAP"));
@@ -173,38 +173,45 @@ public class AutenticacionAction extends ActionSupport implements SessionAware {
 		logger.debug("PasswordLDAP=" +   this.getText("validacion.ldap.PasswordLDAP"));
 		logger.debug("SearchBaseLDAP=" + this.getText("validacion.ldap.SearchBaseLDAP"));
 		
-		Hashtable env = datosConexionLDAP(this.getText("validacion.ldap.UsuarioLDAP"),
-				this.getText("validacion.ldap.PasswordLDAP"));
+		Hashtable env = datosConexionLDAP(this.getText("validacion.ldap.UsuarioLDAP"), this.getText("validacion.ldap.PasswordLDAP"));
 		boolean existeUsuario = false;
 		try {
-			DirContext ctx = new InitialLdapContext(env, null);
-			SearchControls searchCtls = new SearchControls();
-			String returnedAtts[] = { "uid", "sn", "givenName", "mail", "cn" };
-			searchCtls.setReturningAttributes(returnedAtts);
-			searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-			String searchFilter = "(cn=" + user.toLowerCase() + ")";
-			// String searchBase = "cn=Users,dc=biosnettcs,dc=com";
-			NamingEnumeration<SearchResult> results = ctx.search(
-					this.getText("validacion.ldap.SearchBaseLDAP"), searchFilter, searchCtls);
-			while (results.hasMoreElements()) {
-				SearchResult searchResult = (SearchResult) results.next();
-				Attributes attrs = searchResult.getAttributes();
-				// OBTENEMOS LA UNIDAD ORGANIZATIVA DEL UID BUSCADO CON SU UID Y
-				// LO COMPLETAMOS CON LA BASE
-				String dn = searchResult.getName() + ","
-						+ this.getText("validacion.ldap.SearchBaseLDAP");
-
-				if (attrs != null) {
-					// EL UID EXISTE AHORA VALIDAR PASSWORD
-					existeUsuario = validarAuth(dn, password);
-					// SI VALIDO ES false PASSWORD INCORRECTO, SI ES true
-					// PASSWORD CORRECTO
-				}
-			}
-			ctx.close();
+			 ctx = new InitialLdapContext(env, null);
 		} catch (NamingException e) {
-			logger.error("Error en el proceso de validacion de usuario", e);
+			throw new Exception("Error de conexi&oacute;n",e);
+			//logger.error("Error en el proceso de validacion de usuario", e);
 		}
+			
+		try{
+				SearchControls searchCtls = new SearchControls();
+				String returnedAtts[] = { "uid", "sn", "givenName", "mail", "cn" };
+				searchCtls.setReturningAttributes(returnedAtts);
+				searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+				String searchFilter = "(cn="+user+")";
+				// String searchBase = "cn=Users,dc=biosnettcs,dc=com";
+				NamingEnumeration<SearchResult> results = ctx.search(
+						this.getText("validacion.ldap.SearchBaseLDAP"), searchFilter, searchCtls);
+				while (results.hasMoreElements()) {
+					SearchResult searchResult = (SearchResult) results.next();
+					Attributes attrs = searchResult.getAttributes();
+					// OBTENEMOS LA UNIDAD ORGANIZATIVA DEL UID BUSCADO CON SU UID Y
+					// LO COMPLETAMOS CON LA BASE
+					String dn = searchResult.getName() + ","
+							+ this.getText("validacion.ldap.SearchBaseLDAP");
+	
+					if (attrs != null) {
+						// EL UID EXISTE AHORA VALIDAR PASSWORD
+						existeUsuario = validarAuth(dn, password);
+						// SI VALIDO ES false PASSWORD INCORRECTO, SI ES true
+						// PASSWORD CORRECTO
+					}
+				}
+				ctx.close();
+			}
+			catch(Exception err)
+			{
+				logger.error("Error en el proceso de validacion de usuario", err);
+			}
 		return existeUsuario;
 	}
 
