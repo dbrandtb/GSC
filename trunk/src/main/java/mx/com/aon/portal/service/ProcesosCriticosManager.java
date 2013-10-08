c8d2b273678a0721bcf9c2eddd951f0466521aa3
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2013-10-08 23:43:59 +0000

    Limpieza de proyecto (Struts2, properties y beans de Spring).

--- trunk/src/main/java/mx/com/aon/portal/service/ProcesosCriticosManager.java
+++
@@ -1,9 +0,0 @@
-package mx.com.aon.portal.service;
-
-import mx.com.aon.portal.model.MensajesVO;
-import mx.com.gseguros.exception.ApplicationException;
-
-public interface ProcesosCriticosManager {
-
-	public MensajesVO confirmaPassword(String usuario, String password) throws ApplicationException;
-}
