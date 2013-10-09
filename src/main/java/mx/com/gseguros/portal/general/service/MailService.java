2af52ccf1fdd217e453ef1c1a906ad9d548f5cbd
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-10-09 06:12:28 +0000

    Modificaión para envío de mails a varios destinatarios en "To"

--- src/main/java/mx/com/gseguros/portal/general/service/MailService.java
+++ src/main/java/mx/com/gseguros/portal/general/service/MailService.java
@@ -1,21 +1,20 @@
 package mx.com.gseguros.portal.general.service;
 
-import java.util.List;
 
 public interface MailService {
 	
 	/**
 	 * Envio de correo
-	 * 
-	 * @param rutaArchivos Lista con las rutas locales o rutas http de los archivos
 	 * @param to
 	 * @param cc
 	 * @param bcc
 	 * @param asunto
 	 * @param mensaje
-	 * @param rutaArchivos
+	 * @param rutasAdjuntos rutas de los archivos a adjuntar
 	 * @return true si envió el correo, false si no lo envió
 	 */
-	public boolean enviaCorreo(String to, List<String> cc, List<String> bcc, String asunto, String mensaje, List<String> rutaArchivos);
+	public boolean enviaCorreo(String[] to, String[] cc, String[] bcc,
+			String asunto, String mensaje, String[] rutasAdjuntos);
+	
 
 }
