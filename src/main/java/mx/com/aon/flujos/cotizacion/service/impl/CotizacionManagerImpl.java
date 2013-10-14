59437d25d6c0c5789099cfaeecbda9465dbba87b
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-10-14 21:45:51 +0000

    Limpieza de proyecto (clases Java).

--- src/main/java/mx/com/aon/flujos/cotizacion/service/impl/CotizacionManagerImpl.java
+++ src/main/java/mx/com/aon/flujos/cotizacion/service/impl/CotizacionManagerImpl.java
@@ -1,7 +1,5 @@
 package mx.com.aon.flujos.cotizacion.service.impl;
 
-import static mx.com.aon.portal.dao.CotizacionDAO.OBTIENE_RESULTADOS;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -9,167 +7,25 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import mx.com.aon.configurador.pantallas.model.PantallaVO;
 import mx.com.aon.configurador.pantallas.model.components.ColumnGridVO;
-import mx.com.aon.configurador.pantallas.model.components.ComboClearOnSelectVO;
 import mx.com.aon.configurador.pantallas.model.components.GridVO;
 import mx.com.aon.configurador.pantallas.model.components.ItemVO;
 import mx.com.aon.configurador.pantallas.model.components.RecordVO;
-import mx.com.aon.flujos.cotizacion.model.ObjetoCotizacionVO;
 import mx.com.aon.flujos.cotizacion.model.ResultadoCotizacionVO;
-import mx.com.aon.flujos.cotizacion.service.CotizacionPrincipalManager;
-import mx.com.aon.portal.model.BaseObjectVO;
-import mx.com.aon.portal.model.CotizacionMasivaVO;
-import mx.com.aon.portal.service.impl.AbstractManagerJdbcTemplateInvoke;
-import mx.com.aon.portal.util.WrapperResultados;
-import mx.com.gseguros.exception.ApplicationException;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
-public class CotizacionManagerImpl extends AbstractManagerJdbcTemplateInvoke implements CotizacionPrincipalManager{
+public class CotizacionManagerImpl {
 	
 	protected final transient Logger log = Logger.getLogger(CotizacionManagerImpl.class);
 	
-	@SuppressWarnings("unchecked")
-	public List<CotizacionMasivaVO> obtineTvalositCotiza(String cdunieco,String poliza,String cdramo,String estado )throws ApplicationException {
-	
-	Map map = new HashMap();
-		map.put("pv_cdunieco_i", cdunieco);
-		map.put("pv_nmpoliza_i", poliza);
-		map.put("pv_cdramo_i", cdramo);
-		map.put("pv_estado_i", estado);
-	   log.debug("CotizacionManagerImpl.obtineTvalositCotiza params= " + map);
-		
-		String endpoindName = "P_OBTIENE_TVALOSIT_COTIZA";
-		return getAllBackBoneInvoke(map, endpoindName );
-	}
-	
-	public PantallaVO getPantallaFinal(Map<String, String> parameters) throws ApplicationException {
-
-		PantallaVO pantallaVO = new PantallaVO();
-		try {
-			/*Endpoint endpoint = (Endpoint) endpoints.get("OBTIENE_PANTALLA_FINAL");
-			pantallaVO = (PantallaVO) endpoint.invoke(parameters);*/
-			
-			pantallaVO = (PantallaVO) getBackBoneInvoke(parameters, "OBTIENE_PANTALLA_FINAL");
-			if(logger.isDebugEnabled()){
-				logger.debug("getPantallaFinal()   pantallaVO=" +pantallaVO);
-			}
-		} catch (Exception bae) {
-			logger.error("Exception in invoke getPantallaFinal en cotizacion.. ",bae);
-			throw new ApplicationException("Error intentando obtener pantalla final");
-		}
-		return pantallaVO;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<ComboClearOnSelectVO> getComboPadre(Map<String, String> parameters) throws ApplicationException{
-		List<ComboClearOnSelectVO> lccosvo = new ArrayList<ComboClearOnSelectVO>();
-		try {
-			/*Endpoint endpoint = (Endpoint) endpoints.get("GET_COMBO_PADRE");
-			lccosvo = (List<ComboClearOnSelectVO>) endpoint.invoke(parameters);*/
-			WrapperResultados res = returnBackBoneInvoke(parameters, "GET_COMBO_PADRE");
-			lccosvo = res.getItemList();
-			
-		} catch (Exception bae) {
-			logger.error("Exception en obtener combos padres... ", bae);
-			throw new ApplicationException("Error en obtener combos padres");
-		}
-		return lccosvo;
-		
-	}
-	
-	
-	@SuppressWarnings("unchecked")
-	public BaseObjectVO getEtiqueta(String cdramo, String cdtipsit)throws ApplicationException {
-		Map params= new HashMap<String, String>();
-		params.put("PV_CDRAMO_I", cdramo);
-		params.put("PV_CDTIPSIT_I", cdtipsit);
-		BaseObjectVO etiqueta = new BaseObjectVO();
-		etiqueta.setValue("");
-		try {
-			/*Endpoint endpoint = (Endpoint)endpoints.get("OBTIENE_ETIQUETA_PRODUCTO_ESPECIAL");
-			etiqueta= (BaseObjectVO) endpoint.invoke(params);*/
-			WrapperResultados result = returnBackBoneInvoke(params, "OBTIENE_ETIQUETA_PRODUCTO_ESPECIAL");
-			if(result.getItemMap() != null){
-				if(result.getItemMap().containsKey("ETIQUETA")) {
-					String etiquetaTmp = (String)result.getItemMap().get("ETIQUETA");
-					if(etiquetaTmp!=null) etiqueta.setLabel(etiquetaTmp);
-				}
-			}
-			
-		} catch (Exception e) {
-			logger.error("ERROR getequipo: "+ e.getMessage(),e);
-			throw new ApplicationException("Error retrieving data label");
-		}
-		return etiqueta;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<ObjetoCotizacionVO> getListaEquipo(ObjetoCotizacionVO objetoCotizacionVO) throws ApplicationException {
-		List<ObjetoCotizacionVO> resultadoObjeto = null;
-		Map params= new HashMap<String, String>();
-		params.put("PV_CDUNIECO", objetoCotizacionVO.getCdUnieco());
-		params.put("PV_CDRAMO", objetoCotizacionVO.getCdRamo());
-		params.put("PV_ESTADO", objetoCotizacionVO.getEstado());
-		params.put("PV_NMPOLIZA", objetoCotizacionVO.getNmPoliza());
-		params.put("PV_NMSITUAC", objetoCotizacionVO.getNmSituac());
-		params.put("PV_NMOBJETO", null);
-		
-		try {
-			/*Endpoint endpoint = (Endpoint)endpoints.get("OBTIENE_LISTA_COTIZACION_ESPECIAL");
-			resultadoObjeto = (List<ObjetoCotizacionVO>) endpoint.invoke(objetoCotizacionVO);*/
-			WrapperResultados res = returnBackBoneInvoke(params, "OBTIENE_LISTA_COTIZACION_ESPECIAL");
-			resultadoObjeto = res.getItemList();
-			
-		} catch (Exception bae) {
-			logger.error("ERROR getequipo: "+ bae.getMessage(),bae);
-			throw new ApplicationException("Error retrieving data");			
-		}
-		return (List<ObjetoCotizacionVO>)resultadoObjeto;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<BaseObjectVO> getTipos(String cdramo, String cdtipsit)throws ApplicationException {
-		Map params= new HashMap<String, String>();
-		params.put("PV_CDRAMO_I", cdramo);
-		params.put("PV_CDTIPSIT_I", cdtipsit);
-		List<BaseObjectVO> tipos= null;
-		try {
-			/*Endpoint endpoint = (Endpoint)endpoints.get("OBTIENE_lISTA_TIPO_PRODUCTO_ESPECIAL");
-			tipos= (List<BaseObjectVO>) endpoint.invoke(params);*/
-			
-			WrapperResultados res = returnBackBoneInvoke(params, "OBTIENE_lISTA_TIPO_PRODUCTO_ESPECIAL");
-			tipos = res.getItemList();
-			if(logger.isDebugEnabled())logger.debug("Tipos producto especial: "+ tipos);
-			
-		} catch (Exception e) {
-			logger.error("ERROR get tipos: "+ e.getMessage(),e);
-			throw new  ApplicationException("Error retrieving data list");
-		}
-		return (List<BaseObjectVO>)tipos;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public GridVO getResultados(Map<String, String> parameters) throws ApplicationException {
-		List <ResultadoCotizacionVO> resultList = null;
-		GridVO gridVo = null;
-		try {
-		    if(logger.isDebugEnabled())logger.debug("OBTIENE_RESULTADOS parameters=" + parameters);
-		    
-			WrapperResultados res = returnBackBoneInvoke(parameters, OBTIENE_RESULTADOS);
-		    resultList = res.getItemList();
-		    
-		    gridVo = adaptarDatosCotizacion(resultList);
-		    
-		} catch (Exception e) {
-			logger.error("Exception in invoke obtener resultados en cotizacion.. ",e);
-			throw new ApplicationException("Error intentando obtener resultados en cotizacion");
-		}
-		return gridVo;
-	}
-	
+	/**
+	 * Metodo antiguo utilizado para adaptar los datos de cotizacion y mostrarlos en un grid
+	 * @param resultList
+	 * @return GridVO con los resultados
+	 * @throws Exception
+	 */
 	public GridVO adaptarDatosCotizacion(List <ResultadoCotizacionVO> resultList) throws Exception {
 		GridVO gridVo = new GridVO();
 		if(resultList == null) return gridVo;
