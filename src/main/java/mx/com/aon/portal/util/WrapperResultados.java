59d44b36c769d2e7161e7bcd1935d39c598ccb05
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-11-14 01:39:04 +0000

    Limpieza de codigo

--- src/main/java/mx/com/aon/portal/util/WrapperResultados.java
+++ src/main/java/mx/com/aon/portal/util/WrapperResultados.java
@@ -9,134 +9,120 @@ import java.util.StringTokenizer;
 import org.apache.commons.lang.StringUtils;
 
 public class WrapperResultados implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    /** tipos de error */
+	private static final String MSGTITLE_ERROR = "1";
+	private static final String MSGTITLE_INFO = "2";
+	private static final String MSGTITLE_INTERROG = "3";
+     
+	/** identificador del mensaje resultante de la operacion **/
+	private String msgId;
+ 
+	/** Tipo de mensaje o error, resultante de la operacion **/
+	private String msgTitle;
 	
-	 /** tipos de error */  
-	 private static final String MSGTITLE_ERROR = "1";
-	 private static final String MSGTITLE_INFO = "2";
-	 private static final String MSGTITLE_INTERROG = "3";
+	private String msgText;
+	
+	/**En caso de estar paginado el resultado de la lista itemList, almacena el total de la lista original sin paginar */
+	int notPagedTotalItems;
+	
+	/** Lista de resultados de consultas o cualquier cursor de salida */
+	@SuppressWarnings("unchecked")
+	private List itemList;
 	 
-	 /** identificador del mensaje resultante de la operacion **/
-	 private String msgId;
-	 
-	 /** Tipo de mensaje o error, resultante de la operacion **/
-	 private String msgTitle;
-
-     private String msgText;
-
-     /**En caso de estar paginado el resultado de la lista itemList, almacena el total de la lista original
-	  sin paginar */
-	 int notPagedTotalItems;
-
-	 /** Lista de resultados de consultas o cualquier cursor de salida */
-	 @SuppressWarnings("unchecked")
-	 private List itemList;
-	 
-	 /** Mapa para colocar cualquier otro parametro de salida */
-	 private Map<String,Object> itemMap; 
-
-
-	 private static final long serialVersionUID = 1L;
-
-     // utiilado en algunos plsql ej: validar_existe_tarea
-     private String resultado;
+	/** Mapa para colocar cualquier otro parametro de salida */
+	private Map<String,Object> itemMap; 
+     
      
      /**
-	  * Permite saber si el mensaje recibido fue de error
-	  */
-	 public boolean isMsgError(){
-		 return this.msgTitle.equals(MSGTITLE_ERROR);
-	 }
+      * Permite saber si el mensaje recibido fue de error
+      */
+     public boolean isMsgError(){
+         return this.msgTitle.equals(MSGTITLE_ERROR);
+     }
      /**
-	  * Permite saber si el mensaje recibido fue de error
-	  */
-	 public boolean isMsgInformation(){
-		 return this.msgTitle.equals(MSGTITLE_INFO);
-	 }
+      * Permite saber si el mensaje recibido fue de error
+      */
+     public boolean isMsgInformation(){
+         return this.msgTitle.equals(MSGTITLE_INFO);
+     }
      /**
-	  * Permite saber si el mensaje recibido fue de error
-	  */
-	 public boolean isMsgInterrogation(){
-		 return this.msgTitle.equals(MSGTITLE_INTERROG);
-	 }	 
-	 
-	 /**
-	  * @return the msgId
-	  */
-	 public String getMsgId() {
-		 
-	  if(StringUtils.isNotBlank(msgId)){
-		   StringTokenizer tokens = new StringTokenizer(msgId);  
-		   if(tokens.hasMoreTokens()){  
-			   msgId = tokens.nextToken();  
-		   }  
-	  }
-	  
-	  return msgId;
-	 }
+      * Permite saber si el mensaje recibido fue de error
+      */
+     public boolean isMsgInterrogation(){
+         return this.msgTitle.equals(MSGTITLE_INTERROG);
+     }     
+     
+     /**
+      * @return the msgId
+      */
+     public String getMsgId() {
+         
+      if(StringUtils.isNotBlank(msgId)){
+           StringTokenizer tokens = new StringTokenizer(msgId);  
+           if(tokens.hasMoreTokens()){  
+               msgId = tokens.nextToken();  
+           }  
+      }
+      
+      return msgId;
+     }
 
-	 /**
-	  * @param msgId the msgId to set
-	  */
-	 public void setMsgId(String msgId) {
-	  this.msgId = msgId;
-	 }
-
-
-	 /**
-	  * @return the itemList
-	  */
-	 @SuppressWarnings("unchecked")
-	public List getItemList() {
-	  return itemList;
-	 }
-
-	 /**
-	  * @param itemList the itemList to set
-	  */
-	 @SuppressWarnings("unchecked")
-	public void setItemList(List itemList) {
-	  this.itemList = itemList;
-	 }
-
-	 /**
-	  * @return the itemMap
-	  */
-	 public Map<String, Object> getItemMap() {
-	  return itemMap;
-	 }
-
-	 /**
-	  * @param itemMap the itemMap to set
-	  */
-	 public void setItemMap(Map<String, Object> itemMap) {
-	  this.itemMap = itemMap;
-	 }
-
-	public String getMsgTitle() {
-		return msgTitle;
-	}
-
-	public void setMsgTitle(String msgTitle) {
-		this.msgTitle = msgTitle;
-	}
-
-	public int getNotPagedTotalItems() {
-		return notPagedTotalItems;
-	}
-
-	public void setNotPagedTotalItems(int notPagedTotalItems) {
-		this.notPagedTotalItems = notPagedTotalItems;
-	}
+     /**
+      * @param msgId the msgId to set
+      */
+     public void setMsgId(String msgId) {
+      this.msgId = msgId;
+     }
 
 
-    public String getResultado() {
-        return resultado;
+     /**
+      * @return the itemList
+      */
+     @SuppressWarnings("unchecked")
+    public List getItemList() {
+      return itemList;
+     }
+
+     /**
+      * @param itemList the itemList to set
+      */
+     @SuppressWarnings("unchecked")
+    public void setItemList(List itemList) {
+      this.itemList = itemList;
+     }
+
+     /**
+      * @return the itemMap
+      */
+     public Map<String, Object> getItemMap() {
+      return itemMap;
+     }
+
+     /**
+      * @param itemMap the itemMap to set
+      */
+     public void setItemMap(Map<String, Object> itemMap) {
+      this.itemMap = itemMap;
+     }
+
+    public String getMsgTitle() {
+        return msgTitle;
     }
 
-    public void setResultado(String resultado) {
-        this.resultado = resultado;
+    public void setMsgTitle(String msgTitle) {
+        this.msgTitle = msgTitle;
     }
 
+    public int getNotPagedTotalItems() {
+        return notPagedTotalItems;
+    }
+
+    public void setNotPagedTotalItems(int notPagedTotalItems) {
+        this.notPagedTotalItems = notPagedTotalItems;
+    }
 
     public String getMsgText() {
         return msgText;
