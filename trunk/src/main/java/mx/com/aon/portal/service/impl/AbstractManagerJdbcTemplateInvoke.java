927ff2f99691fc7575e841cddfe9ac7225c1c0ac
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2013-11-14 23:32:28 +0000

    Se eliminan métodos no utilizados.

--- trunk/src/main/java/mx/com/aon/portal/service/impl/AbstractManagerJdbcTemplateInvoke.java
+++ trunk/src/main/java/mx/com/aon/portal/service/impl/AbstractManagerJdbcTemplateInvoke.java
@@ -3,7 +3,6 @@ package mx.com.aon.portal.service.impl;
 import java.util.List;
 
 import mx.com.aon.portal.dao.AbstractDAO;
-import mx.com.aon.portal.service.PagedList;
 import mx.com.aon.portal.service.ProcessResultManagerJdbcTemplate;
 import mx.com.aon.portal.util.WrapperResultados;
 import mx.com.gseguros.exception.ApplicationException;
@@ -33,135 +32,6 @@ public abstract class AbstractManagerJdbcTemplateInvoke {
     public void setAbstractDAO(AbstractDAO abstractDAO) {
         this.abstractDAO = abstractDAO;
     }
-
-    /**
-	 * Metodo que ejecuta backbone y obtiene una lista filtrada de el cursor de salida obtenido
-	 *
-	 * Nota: Este metodo existe como alternativa a la carencia de paginacion por parte de backbone (o para acceder a los resultset)
-	 * y porque en el proyecto actual solo se invocan stored procedures q no reciben ningun parametro de paginacion.
-	 *
-	 * @param arg Object a pasar a metodo invoke de backbone
-	 * @param endpointName nombre del endpoint a invocar
-	 * @param start  desde , paginacion
-	 * @param limit  hasta , paginacion
-	 * @return instancia de WrapperResultados cargado con los id de msgs de respuesta y resultados
-	 * @throws mx.com.gseguros.exception.ApplicationException
-     */
-	@SuppressWarnings("unchecked")
-	public PagedList pagedBackBoneInvoke(Object arg,String endpointName ,int start,int limit) throws ApplicationException {
-		WrapperResultados res;
-
-		try {
-
-            res = (WrapperResultados) abstractDAO.invoke(endpointName,arg);
-
-        } catch (DaoException e) {
-			logger.error("No se puedo ejecutar pagedBackBoneInvoke para el endpoint " + endpointName, e);
-			throw new ApplicationException("No se puedo ejecutar pagedBackBoneInvoke para el endpoint " + endpointName,e);
-		}
-        
-        PagedList pagedList = new PagedList();
-        pagedList.setItemMap(res.getItemMap());
-        
-        if (res.getItemList()==null) {
-            logger.debug("No se encontraron datos");
-            throw new ApplicationException("No se encontraron datos");
-        }
-        if (res.getItemList().size()==0) {
-            logger.debug("No se encontraron datos");
-            throw new ApplicationException("No se encontraron datos");
-        }
-        //carga el tamanio antes de filtrar la lista
-		res.setNotPagedTotalItems( res.getItemList().size() );
-		
-		
-		//Si limit es -1 regresa todos los elementos (esto para el caso en el que se reutilizan los metodos en combos y no en pagedGrids 
-		if(limit != -1){
-	        if (res.getItemList().size() < (start + limit)) {
-	          limit =  res.getItemList().size();
-	        }  else {
-	          limit = start + limit;
-	        }
-		
-	        //sobrescribe la lista con una sublista
-			res.setItemList( res.getItemList().subList(start, limit) );
-		}
-        
-        
-        res = processResultManager.processResultMessageId(res);
-
-        pagedList.setItemsRangeList(res.getItemList());
-        pagedList.setTotalItems(res.getNotPagedTotalItems());
-        pagedList.setMessageResult(res.getMsgText());
-        
-
-        return pagedList;
-
-	}
-
-
-	/**
-	 * Metodo que ejecuta backbone y obtiene una lista filtrada de el cursor de salida obtenido
-	 *
-	 * Nota: Este metodo existe como alternativa a la carencia de paginacion por parte de backbone (o para acceder a los resultset)
-	 * y porque en el proyecto actual solo se invocan stored procedures q no reciben ningun parametro de paginacion.
-	 *
-	 * @param arg Object a pasar a metodo invoke de backbone
-	 * @param endpointName nombre del endpoint a invocar
-	 * @param start  desde , paginacion
-	 * @param limit  hasta , paginacion
-	 * @return instancia de WrapperResultados cargado con los id de msgs de respuesta y resultados
-	 * @throws mx.com.gseguros.exception.ApplicationException
-     */
-	@SuppressWarnings("unchecked")
-	public PagedList returnAllPagedBackBoneInvoke(Object arg,String endpointName ,int start,int limit) throws ApplicationException {
-		WrapperResultados res;
-
-		try {
-
-            res = (WrapperResultados) abstractDAO.invoke(endpointName,arg);
-
-        } catch (DaoException e) {
-			logger.error("No se puedo ejecutar pagedBackBoneInvoke para el endpoint " + endpointName, e);
-			throw new ApplicationException("No se puedo ejecutar pagedBackBoneInvoke para el endpoint " + endpointName,e);
-		}
-        
-        PagedList pagedList = new PagedList();
-        pagedList.setItemMap(res.getItemMap());
-        
-        if (res.getItemList()==null || res.getItemList().isEmpty()) {
-            logger.debug("No se encontraron datos");
-            pagedList.setTotalItems(0);
-	        pagedList.setMessageResult("No se encontraron datos");
-            
-        }else {
-	        //carga el tamanio antes de filtrar la lista
-			res.setNotPagedTotalItems( res.getItemList().size() );
-			
-			
-			//Si limit es -1 regresa todos los elementos (esto para el caso en el que se reutilizan los metodos en combos y no en pagedGrids 
-			if(limit != -1){
-		        if (res.getItemList().size() < (start + limit)) {
-		          limit =  res.getItemList().size();
-		        }  else {
-		          limit = start + limit;
-		        }
-			
-		        //sobrescribe la lista con una sublista
-				res.setItemList( res.getItemList().subList(start, limit) );
-			}
-	        
-	        
-	        res = processResultManager.processResultMessageId(res);
-	
-	        pagedList.setItemsRangeList(res.getItemList());
-	        pagedList.setTotalItems(res.getNotPagedTotalItems());
-	        pagedList.setMessageResult(res.getMsgText());
-	        
-        }
-        return pagedList;
-	}
-
 	
     /**
      * Metodo que ejecuta backbone y obtiene un registro dado un conjunto de argumentos
@@ -202,32 +72,6 @@ public abstract class AbstractManagerJdbcTemplateInvoke {
         } else {
             throw new ApplicationException("Registro no encontrado");
         }
-    }
-
-    /**
-     * Metodo que ejecuta backbone y obtiene un registro dado un conjunto de argumentos
-     *
-     *
-     * @param arg Object a pasar a metodo invoke de backbone
-     * @param endpointName nombre del endpoint a invocar
-     * @return instancia de WrapperResultados cargado con los id de msgs de respuesta y resultados
-     * @throws mx.com.gseguros.exception.ApplicationException
-     */
-    @SuppressWarnings("unchecked")
-    public void voidReturnBackBoneInvoke(Object arg,String endpointName ) throws ApplicationException {
-        WrapperResultados res;
-
-        try {
-
-            res = (WrapperResultados) abstractDAO.invoke(endpointName,arg);
-
-        } catch (DaoException e) {
-            logger.error("No se puedo ejecutar pagedBackBoneInvoke para el endpoint " + endpointName, e);
-            throw new ApplicationException("No se puedo ejecutar pagedBackBoneInvoke para el endpoint " + endpointName,e);
-        }
-
-        res = processResultManager.processResultMessageId(res);
-
     }
 
     /**
@@ -311,44 +155,4 @@ public abstract class AbstractManagerJdbcTemplateInvoke {
 
     }
 
-    @SuppressWarnings("unchecked")
-    public List getExporterAllBackBoneInvoke(Object arg,String endpointName) throws ApplicationException {
-        WrapperResultados res;
-
-        try {
-
-            res = (WrapperResultados) abstractDAO.invoke(endpointName,arg);
-
-        } catch (DaoException e) {
-            logger.error("No se puedo ejecutar pagedBackBoneInvoke para el endpoint " + endpointName, e);
-            return null;
-        }
-
-        if (res.getItemList()==null) {
-            logger.debug("No se encontraron datos");
-            return null;
-        }
-
-
-        res = processResultManager.processResultMessageId(res);
-
-
-        return res.getItemList();
-
-    }
-
-
-    /**
-     * Metodo que solo procesa el mensaje final apartir de un msgId contenido en un WrapperResultados
-     *
-     * @param WrapperResultados que tiene en su propiedad msgId el codigo del mensaje a procesar 
-     * @return instancia de WrapperResultados cargado con el msgTitle, msgText procesados por el msgId enviado
-     * @throws mx.com.gseguros.exception.ApplicationException
-     */
-    @SuppressWarnings("unchecked")
-    public WrapperResultados returnProcessResultMessageId(WrapperResultados res) throws ApplicationException {
-        res = processResultManager.processResultMessageId(res);
-        return res;
-
-    }
 }
