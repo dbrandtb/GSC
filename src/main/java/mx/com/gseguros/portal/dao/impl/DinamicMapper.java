402ca6c5dcf482574b00e593ec890fc265b9f8fc
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-12-18 00:52:31 +0000

    MAPEADOR QUE IDENTIFICA DE MANERA AUTOMATICA LAS COLUMNAS

---
+++ src/main/java/mx/com/gseguros/portal/dao/impl/DinamicMapper.java
@@ -0,0 +1,42 @@
+package mx.com.gseguros.portal.dao.impl;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import mx.com.gseguros.utils.Utilerias;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import org.apache.log4j.Logger;
+
+public class DinamicMapper implements RowMapper
+{
+	
+	private static Logger log=Logger.getLogger(DinamicMapper.class);
+
+	@Override
+	public Object mapRow(ResultSet rs, int rowNum) throws SQLException
+	{
+		Map<String,String> map=new HashMap<String,String>(0);
+		ResultSetMetaData metaData = rs.getMetaData();
+		int numCols=metaData.getColumnCount();
+		for (int i=1;i<=numCols;i++)
+		{
+			String col=metaData.getColumnName(i);
+			//log.debug("columna: "+col);
+			if(col!=null&&col.substring(0,2).equalsIgnoreCase("fe"))
+			{
+				map.put(col,Utilerias.formateaFecha(rs.getString(col)));
+			}
+			else
+			{
+				map.put(col,rs.getString(col));
+			}			
+		}
+		return map;
+	}
+	
+}
