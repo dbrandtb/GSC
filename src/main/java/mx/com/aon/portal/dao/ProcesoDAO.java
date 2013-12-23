131555a9d85463c937e3dba3670b0395f57cbcb5
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2013-12-23 18:17:18 +0000

    Se agrega Nmsuplem a PL de Consulta datos Cliente para la insersion en WS, para que se pueda reutilizar en los edosos

--- src/main/java/mx/com/aon/portal/dao/ProcesoDAO.java
+++ src/main/java/mx/com/aon/portal/dao/ProcesoDAO.java
@@ -3450,6 +3450,7 @@ protected class ActualizaValoresSituaciones extends CustomStoredProcedure {
     		declareParameter(new SqlParameter("pv_cdramo_i", OracleTypes.VARCHAR));			
     		declareParameter(new SqlParameter("pv_estado_i", OracleTypes.VARCHAR));			
     		declareParameter(new SqlParameter("pv_nmpoliza_i", OracleTypes.VARCHAR));			
+    		declareParameter(new SqlParameter("pv_nmsuplem_i", OracleTypes.VARCHAR));			
     		declareParameter(new SqlOutParameter("pv_registro_o", OracleTypes.CURSOR, new ClienteMapper()));
     		declareParameter(new SqlOutParameter("pv_msg_id_o", OracleTypes.VARCHAR));
     		declareParameter(new SqlOutParameter("pv_title_o", OracleTypes.VARCHAR));
