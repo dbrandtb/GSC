5d06dcce47d170e0db9dc10ceacbfc6e6b7c7529
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2014-02-11 20:54:26 +0000

    EDITOR DE PANTALLAS CON RESPALDO ANTI CRASH

--- trunk/src/main/java/mx/com/gseguros/portal/general/dao/PantallasDAO.java
+++ trunk/src/main/java/mx/com/gseguros/portal/general/dao/PantallasDAO.java
@@ -7,22 +7,18 @@ import mx.com.gseguros.portal.general.model.ComponenteVO;
 
 public interface PantallasDAO
 {
-	//public List<Tatri>              obtenerCamposPantalla      (Map<String,Object> params) throws Exception;
 	/**
 	 * PKG_CONF_PANTALLAS.P_GET_TCONFCMP
 	 */
 	public List<ComponenteVO>       obtenerComponentes         (Map<String,String> params) throws Exception;
-	//public List<Map<String,String>> obtenerParametrosPantalla  (Map<String,Object> params) throws Exception;
 	/**
 	 * PKG_CONF_PANTALLAS.P_GET_TCONFCMP
 	 */
 	public List<Map<String,String>> obtenerParametros          (Map<String,String> params) throws Exception;
-	//public void                     borrarParametrosPantalla   (Map<String,Object> params) throws Exception;
 	/**
-	 * PKG_CONF_PANTALLAS.P_BORRAR_TCONFCMP
+	 * PKG_CONF_PANTALLAS.P_MOV_TCONFCMP
 	 */
-	public void                     borrarParametros           (Map<String,String> params) throws Exception;
-	//public void                     insertarParametrosPantalla (Map<String,String> params) throws Exception;
+	public void                     movParametros           (Map<String,String> params) throws Exception;
 	/**
 	 * PKG_CONF_PANTALLAS.P_INSERTA_TCONFCMP
 	 */
