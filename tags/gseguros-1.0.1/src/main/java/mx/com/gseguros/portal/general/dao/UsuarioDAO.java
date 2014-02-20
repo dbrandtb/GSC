544a110dea5a846c13cf7dec2c6737c468c6fe39
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2014-02-20 03:57:45 +0000

    Version 1.0.1 de gseguros, que contiene la parte de Endosos.

---
+++ tags/gseguros-1.0.1/src/main/java/mx/com/gseguros/portal/general/dao/UsuarioDAO.java
@@ -0,0 +1,24 @@
+package mx.com.gseguros.portal.general.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import mx.com.aon.portal.model.IsoVO;
+import mx.com.aon.portal.model.UsuarioRolEmpresaVO;
+import mx.com.aon.portal2.web.GenericVO;
+import mx.com.gseguros.exception.DaoException;
+import mx.com.gseguros.portal.general.model.RolVO;
+import mx.com.gseguros.portal.general.model.UsuarioVO;
+
+public interface UsuarioDAO {
+
+	public GenericVO guardaUsuario(Map params) throws DaoException;
+
+	public List<UsuarioVO> obtieneUsuarios(Map params) throws DaoException;
+	public List<RolVO> obtieneRolesUsuario(Map params) throws DaoException;
+	
+	public List<UsuarioRolEmpresaVO> obtieneRolesCliente(String user) throws DaoException;
+
+	public IsoVO obtieneVariablesIso(String user) throws DaoException;
+
+}
---
+++ tags/gseguros-1.0.1/src/main/java/mx/com/gseguros/wizard/configuracion/producto/conceptosCobertura/model/ConceptosCoberturaVO.java
@@ -0,0 +1,154 @@
+package mx.com.gseguros.wizard.configuracion.producto.conceptosCobertura.model;
+
+import java.io.Serializable;
+
+public class ConceptosCoberturaVO implements Serializable {
+
+	
+	private static final long serialVersionUID = 7149537862815177617L;
+
+	/**
+	 * Codigo del periodo para el concepto por cobertura.
+	 */
+	private String codigoPeriodo;
+
+	/**
+	 * Descripcion del periodo para concepto por cobertura .
+	 */
+	private String descripcionPeriodo;
+	
+	/**
+	 * Codigo de la cobertura para el concepto por cobertura.
+	 */
+	private String codigoCobertura;
+
+	/**
+	 * Descripcion de la cobertura para concepto por cobertura .
+	 */
+	private String descripcionCobertura;
+	
+	/**
+	 * Codigo del concepto para el concepto por cobertura.
+	 */
+	private String codigoConcepto;
+
+	/**
+	 * Descripcion del concepto para concepto por cobertura .
+	 */
+	private String descripcionConcepto;
+	
+	/**
+	 * Codigo del comportamiento para el concepto por cobertura.
+	 */
+	private String codigoComportamiento;
+
+	/**
+	 * Descripcion del comportamiento para concepto por cobertura .
+	 */
+	private String descripcionComportamiento;
+	
+	/**
+	 * Codigo de la condicion para el concepto por cobertura.
+	 */
+	private String codigoCondicion;
+
+	/**
+	 * Descripcion de la condicion para concepto por cobertura .
+	 */
+	private String descripcionCondicion;
+	
+	/**
+	 * orden para concepto por cobertura.
+	 */
+	private String orden;
+
+	
+	//GETTERS Y SETTERS
+	public String getCodigoPeriodo() {
+		return codigoPeriodo;
+	}
+
+	public void setCodigoPeriodo(String codigoPeriodo) {
+		this.codigoPeriodo = codigoPeriodo;
+	}
+
+	public String getDescripcionPeriodo() {
+		return descripcionPeriodo;
+	}
+
+	public void setDescripcionPeriodo(String descripcionPeriodo) {
+		this.descripcionPeriodo = descripcionPeriodo;
+	}
+
+	public String getCodigoCobertura() {
+		return codigoCobertura;
+	}
+
+	public void setCodigoCobertura(String codigoCobertura) {
+		this.codigoCobertura = codigoCobertura;
+	}
+
+	public String getDescripcionCobertura() {
+		return descripcionCobertura;
+	}
+
+	public void setDescripcionCobertura(String descripcionCobertura) {
+		this.descripcionCobertura = descripcionCobertura;
+	}
+
+	public String getCodigoConcepto() {
+		return codigoConcepto;
+	}
+
+	public void setCodigoConcepto(String codigoConcepto) {
+		this.codigoConcepto = codigoConcepto;
+	}
+
+	public String getDescripcionConcepto() {
+		return descripcionConcepto;
+	}
+
+	public void setDescripcionConcepto(String descripcionConcepto) {
+		this.descripcionConcepto = descripcionConcepto;
+	}
+
+	public String getCodigoComportamiento() {
+		return codigoComportamiento;
+	}
+
+	public void setCodigoComportamiento(String codigoComportamiento) {
+		this.codigoComportamiento = codigoComportamiento;
+	}
+
+	public String getDescripcionComportamiento() {
+		return descripcionComportamiento;
+	}
+
+	public void setDescripcionComportamiento(String descripcionComportamiento) {
+		this.descripcionComportamiento = descripcionComportamiento;
+	}
+
+	public String getCodigoCondicion() {
+		return codigoCondicion;
+	}
+
+	public void setCodigoCondicion(String codigoCondicion) {
+		this.codigoCondicion = codigoCondicion;
+	}
+
+	public String getDescripcionCondicion() {
+		return descripcionCondicion;
+	}
+
+	public void setDescripcionCondicion(String descripcionCondicion) {
+		this.descripcionCondicion = descripcionCondicion;
+	}
+
+	public String getOrden() {
+		return orden;
+	}
+
+	public void setOrden(String orden) {
+		this.orden = orden;
+	}
+}
