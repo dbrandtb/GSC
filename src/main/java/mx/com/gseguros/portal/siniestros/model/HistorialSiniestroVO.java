38a67f4abf2a7cd684a5c552d2f0d881ad4adc21
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2014-03-01 10:22:59 +0000

    Se agrega VO que se usara

---
+++ src/main/java/mx/com/gseguros/portal/siniestros/model/HistorialSiniestroVO.java
@@ -0,0 +1,20 @@
+package mx.com.gseguros.portal.siniestros.model;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+/**
+ * @author Ricardo
+ *
+ */
+public class HistorialSiniestroVO implements Serializable{
+
+	private static final long serialVersionUID = -8555353864912795413L;
+
+	public String toString(){
+		return ToStringBuilder.reflectionToString(this,ToStringStyle.MULTI_LINE_STYLE);
+	}	
+	
+}
