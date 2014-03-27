bff1fd358de28d82a1565227b57e9710c218a82e
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2014-03-27 17:31:11 +0000

    Uso correcto de constantes Calendar (sin instanciar).

--- trunk/src/main/java/mx/com/gseguros/portal/siniestros/dao/impl/SiniestrosDAOImpl.java
+++ trunk/src/main/java/mx/com/gseguros/portal/siniestros/dao/impl/SiniestrosDAOImpl.java
@@ -2361,7 +2361,7 @@ Map<String, Object> mapResult = ejecutaSP(new ObtieneListadoTTAPVAATSP(getDataSo
         	try {
         		logger.debug("--> Parseando hora rs.getString(HOR_OCU) -->> "+ rs.getString("HOR_OCU"));
         		cal = Calendar.getInstance();
-        		cal.set(cal.get(cal.YEAR), cal.get(cal.MONTH), cal.get(cal.DAY_OF_MONTH), rs.getInt("HOR_OCU"), 0);
+        		cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), rs.getInt("HOR_OCU"), 0);
         		logger.debug("--> Calendario obtenido -->> "+ cal);
         		reclamo.setHorOcu(cal);
         	} catch (Exception e) {
@@ -2371,7 +2371,7 @@ Map<String, Object> mapResult = ejecutaSP(new ObtieneListadoTTAPVAATSP(getDataSo
         	try {
         		logger.debug("--> Parseando hora rs.getString(HOR_PRO) -->> "+ rs.getString("HOR_PRO"));
         		cal = Calendar.getInstance();
-        		cal.set(cal.get(cal.YEAR), cal.get(cal.MONTH), cal.get(cal.DAY_OF_MONTH), rs.getInt("HOR_PRO"), 0);
+        		cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), rs.getInt("HOR_PRO"), 0);
         		logger.debug("--> Calendario obtenido -->> "+ cal);
         		reclamo.setHorPro(cal);
         	} catch (Exception e) {
