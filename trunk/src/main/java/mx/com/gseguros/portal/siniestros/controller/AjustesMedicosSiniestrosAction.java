c7dd77b2e0bad001536bf4bf38a5470f3ef38316
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2014-04-10 21:53:17 +0000

    Organize imports.

--- trunk/src/main/java/mx/com/gseguros/portal/siniestros/controller/AjustesMedicosSiniestrosAction.java
+++ trunk/src/main/java/mx/com/gseguros/portal/siniestros/controller/AjustesMedicosSiniestrosAction.java
@@ -3,7 +3,6 @@ package mx.com.gseguros.portal.siniestros.controller;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
