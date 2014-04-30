3c07ec42dc27bc9c467ecaba36c5ed77c7f9596f
HectorLT <HectorLT@68539698-d818-11e6-ad83-4fbd610fe048>
2014-04-30 00:33:10 +0000

    javadoc

--- trunk/src/main/java/mx/com/gseguros/portal/general/service/WebServicesManager.java
+++ trunk/src/main/java/mx/com/gseguros/portal/general/service/WebServicesManager.java
@@ -7,11 +7,28 @@ import mx.com.gseguros.exception.ApplicationException;
 
 public interface WebServicesManager {
 	
-		
+	/**
+	 * Metodo que obtiene la bitacora de WS Fallidos, filtrados por producto, sucursal y poliza
+	 * @param params
+	 * @return
+	 * @throws ApplicationException
+	 */
 	public List<Map<String, String>> obtienePeticionesFallidasWS(Map<String, String> params) throws ApplicationException;
 
+	/**
+	 * Metodo que Otiene el detalle de una peticion Fallida, el cual contiene el cdURL, metodo, Xml a enviar
+	 * @param params
+	 * @return
+	 * @throws ApplicationException
+	 */
 	public List<Map<String, String>> obtieneDetallePeticionWS(Map<String, String> params) throws ApplicationException;
 
+	/**
+	 * Metodo que recibe una lista de peticiones a eliminar
+	 * @param listaEliminar
+	 * @return
+	 * @throws ApplicationException
+	 */
 	public boolean eliminaPeticionWS(List<Map<String, String>> listaEliminar) throws ApplicationException;
 
 }
