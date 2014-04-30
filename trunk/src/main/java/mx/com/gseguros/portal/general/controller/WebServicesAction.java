79173ea7e5a3718d11cf36c5a0b44765a6721a97
HectorLT <HectorLT@68539698-d818-11e6-ad83-4fbd610fe048>
2014-04-30 23:54:53 +0000

    Test Final manual WS sin stub

--- trunk/src/main/java/mx/com/gseguros/portal/general/controller/WebServicesAction.java
+++ trunk/src/main/java/mx/com/gseguros/portal/general/controller/WebServicesAction.java
@@ -70,8 +70,8 @@ public class WebServicesAction extends PrincipalCoreAction{
         		return SUCCESS;	
     		}
     		
-    		OMElement resultadoWS = WebServicesUtil.invocaServicio(urlWS, metodoWS, AXIOMUtil.stringToOM(xmlEnvio), null , null, false, params.get("modo"));
-    		//OMElement resultadoWS = WebServicesUtil.invocaServicioAxis2(urlWS, metodoWS, AXIOMUtil.stringToOM(xmlEnvio), null , null, false, params.get("modo"));
+    		//OMElement resultadoWS = WebServicesUtil.invocaServicio(urlWS, metodoWS, AXIOMUtil.stringToOM(xmlEnvio), null , null, false, params.get("modo"));
+    		OMElement resultadoWS = WebServicesUtil.invocaServicioAxis2(urlWS, metodoWS, AXIOMUtil.stringToOM(xmlEnvio), null , null, false, params.get("modo"));
     		mensajeRespuesta = WebServicesUtil.formatXml(resultadoWS.toString());
     		
     		if("ws.recibossigs.url".equals(peticionWS.get("CDURLWS")) && "generaRecDxn".equalsIgnoreCase(metodoWS)){
