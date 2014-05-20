60615a25fd1c7a70a5dcfb2070a3a18ff1002883
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2014-05-20 22:37:21 +0000

    Organize imports

--- src/main/java/mx/com/gseguros/portal/general/controller/ReportesAction.java
+++ src/main/java/mx/com/gseguros/portal/general/controller/ReportesAction.java
@@ -1,7 +1,6 @@
 package mx.com.gseguros.portal.general.controller;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
