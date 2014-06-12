07955c0a57872cfced48f50767cc8aecd06d07df
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2014-06-12 18:47:55 +0000

    Actualización de valores del enum Ramo.

--- src/main/java/mx/com/gseguros/portal/general/util/Ramo.java
+++ src/main/java/mx/com/gseguros/portal/general/util/Ramo.java
@@ -2,7 +2,9 @@ package mx.com.gseguros.portal.general.util;
 
 public enum Ramo {
 	
-	SALUD_VITAL("2");
+	SALUD_VITAL      ("2"),
+	MULTISALUD       ("4"),
+	AUTOS_FRONTERIZOS("16");
 
 	private String cdramo;
 
