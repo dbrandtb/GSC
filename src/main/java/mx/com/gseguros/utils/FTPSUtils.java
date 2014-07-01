373f3ccc215a024ff1f900c59d501b610441069a
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2014-07-01 21:52:17 +0000

    Se agrega un metoo que recibe el objeto File a subir.

--- src/main/java/mx/com/gseguros/utils/FTPSUtils.java
+++ src/main/java/mx/com/gseguros/utils/FTPSUtils.java
@@ -22,34 +22,36 @@ public class FTPSUtils {
 
 	public static void main(String[] args) {
 		// Prueba de Secure FTP:
-		FTPSUtils.subeArchivo("10.1.1.133", "oinstall", "j4v4n3s", "/export/home/oinstall/software", "E:\\", "CARATULA.pdf");
+		//FTPSUtils.subeArchivo("10.1.1.133", "oinstall", "j4v4n3s", "software", "E:\\", "CARATULA.pdf");
+		FTPSUtils.subeArchivo("10.1.1.133", "oinstall", "j4v4n3s", "software", new File("E:\\CARATULA.pdf"));
 	}
 
-	/**
+	/*
 	 * Sube un archivo v&iacute;a Secure FTP
-	 * @param serverAddress
-	 * @param userId
-	 * @param password
-	 * @param remoteDirectory
-	 * @param localDirectory
-	 * @param fileToFTP
+	 * @param host            Nombre del host que recibir&aacute; el archivo
+	 * @param user            Usuario FTP
+	 * @param password        Password FTP
+	 * @param remoteDirectory Ruta relativa del directorio remoto donde residir&aacute; el archivo
+	 * @param localDirectory  Directorio local
+	 * @param localFileName
 	 * @return true si lo sube correctamente, false si no
 	 */
-	public static boolean subeArchivo(String serverAddress, String userId, String password, 
-			String remoteDirectory, String localDirectory, String fileToFTP) {
+	/*
+	public static boolean subeArchivo(String host, String user, String password, 
+			String remoteDirectory, String localDirectory, String localFileName) {
 		
 		StandardFileSystemManager manager = new StandardFileSystemManager();
 
 		try {
-			logger.debug("serverAddress=" + serverAddress);
-			logger.debug("userId=" + userId);
+			logger.debug("host=" + host);
+			logger.debug("user=" + user);
 			logger.debug("password="+password);
 			logger.debug("remoteDirectory=" + remoteDirectory);
 			logger.debug("localDirectory=" + localDirectory);
-			logger.debug("fileToFTP=" + fileToFTP);
+			logger.debug("fileToFTP=" + localFileName);
 			
 			// check if the file exists
-			String filepath = localDirectory + "/" + fileToFTP;
+			String filepath = localDirectory + "/" + localFileName;
 			File file = new File(filepath);
 			if (!file.exists()) {
 				throw new RuntimeException("Error. Local file not found");
@@ -66,8 +68,75 @@ public class FTPSUtils {
 
 			// Create the SFTP URI using the host name, userid, password, remote
 			// path and file name
-			String sftpUri = "sftp://" + userId + ":" + password + "@"
-					+ serverAddress + "/" + remoteDirectory + "/" + fileToFTP;
+			String sftpUri = "sftp://" + user + ":" + password + "@"
+					+ host + "/" + remoteDirectory + "/" + localFileName;
+
+			// Create local file object
+			FileObject localFile = manager.resolveFile(file.getAbsolutePath());
+
+			logger.debug("localFile=" + localFile);
+			
+			// Create remote file object
+			FileObject remoteFile = manager.resolveFile(sftpUri, opts);
+			
+			logger.debug("remoteFile=" + remoteFile);
+
+			// Copy local file to sftp server
+			remoteFile.copyFrom(localFile, Selectors.SELECT_SELF);
+			logger.info("File upload successful via Secure FTP");
+
+		} catch (Exception ex) {
+			logger.error(ex.getMessage(), ex);
+			return false;
+		} finally {
+			manager.close();
+		}
+		return true;
+	}
+	*/
+	
+	
+	/**
+	 * Sube un archivo v&iacute;a Secure FTP
+	 * @param host
+	 * @param user
+	 * @param password
+	 * @param remoteDirectory
+	 * @param localDirectory
+	 * @param file
+	 * @return true si lo sube correctamente, false si no
+	 */
+	public static boolean subeArchivo(String host, String user, String password, 
+			String remoteDirectory, File file) {
+		
+		StandardFileSystemManager manager = new StandardFileSystemManager();
+
+		try {
+			logger.debug("host=" + host);
+			logger.debug("user=" + user);
+			logger.debug("password="+password);
+			logger.debug("remoteDirectory=" + remoteDirectory);
+			logger.debug("file=" + file.getAbsolutePath());
+			
+			// check if the file exists
+			if (!file.exists()) {
+				throw new RuntimeException("Error. Local file not found");
+			}
+
+			// Initializes the file manager
+			manager.init();
+
+			// Setup our SFTP configuration
+			FileSystemOptions opts = new FileSystemOptions();
+			SftpFileSystemConfigBuilder.getInstance().setStrictHostKeyChecking(opts, "no");
+			SftpFileSystemConfigBuilder.getInstance().setUserDirIsRoot(opts, true);
+			SftpFileSystemConfigBuilder.getInstance().setTimeout(opts, 10000);
+
+			// Create the SFTP URI using the host name, user, password, remote
+			// path and file name
+			String sftpUri = "sftp://" + user + ":" + password + "@"
+					//+ host + "/" + remoteDirectory + "/" + file.getName();
+					+ host + "/" + remoteDirectory + "/" + file.getName();
 
 			// Create local file object
 			FileObject localFile = manager.resolveFile(file.getAbsolutePath());
