c0807dc33ff2776978448593b446b0a7a8be56f2
HectorLT <HectorLT@68539698-d818-11e6-ad83-4fbd610fe048>
2014-07-11 16:15:21 +0000

    CAMBIOS A PRODUCCION PARA CONSULTA DE DOMICILIO CON CDIDEPER Y CDPERSON, PARTE DEL PROCESO QUE GUARDA EL DOMICILIO DEL WS CLIENTE GENERAL

--- trunk/src/main/java/mx/com/aon/portal/dao/ProcesoDAO.java
+++ trunk/src/main/java/mx/com/aon/portal/dao/ProcesoDAO.java
@@ -4372,14 +4372,7 @@ protected class ActualizaValoresSituaciones extends CustomStoredProcedure {
 	protected class BuscarRFCMapper implements RowMapper {
 		public Object mapRow(ResultSet rs, int rowNum) throws SQLException
 		{
-			String cols[]=new String[]{
-					"RFCCLI","NOMBRECLI","FENACIMICLI",
-					"DIRECCIONCLI","CLAVECLI","CDIDEPER",
-					"SEXO", "TIPOPERSONA", "NACIONALIDAD",
-					"NOMBRE", "SNOMBRE", "APPAT",
-					"APMAT","CODPOSTAL","CDEDO","CDMUNICI","DSDOMICIL",
-					"NMNUMERO","NMNUMINT"
-					};
+			String cols[]=new String[]{"RFCCLI","NOMBRECLI","FENACIMICLI","DIRECCIONCLI","CLAVECLI","CDIDEPER", "SEXO", "TIPOPERSONA", "NACIONALIDAD", "NOMBRE", "SNOMBRE", "APPAT", "APMAT"};
 			Map<String,String> map=new HashMap<String,String>(0);
 			for(String col:cols)
 			{
