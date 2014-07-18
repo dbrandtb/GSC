a8d5837091402a251a31216e2e30bcd408fc445a
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2014-07-18 17:51:12 +0000

    SE AGREGAN METODOS. NO ESTAN EN BD PRODUCCION. HACE FALTA SINCRONIZAR 'MODIFICACIONES BD'

--- src/main/java/mx/com/gseguros/portal/cotizacion/dao/CotizacionDAO.java
+++ src/main/java/mx/com/gseguros/portal/cotizacion/dao/CotizacionDAO.java
@@ -1,5 +1,6 @@
 package mx.com.gseguros.portal.cotizacion.dao;
 
+import java.util.List;
 import java.util.Map;
 
 public interface CotizacionDAO
@@ -7,4 +8,10 @@ public interface CotizacionDAO
 	public void movimientoTvalogarGrupo(Map<String,String>params)throws Exception;
 	public void movimientoMpolisitTvalositGrupo(Map<String,String>params)throws Exception;
 	public void movimientoMpoligarGrupo(Map<String,String>params)throws Exception;
+	public Map<String,String>cargarDatosCotizacionGrupo(Map<String,String>params)throws Exception;
+	public List<Map<String,String>>cargarGruposCotizacion(Map<String,String>params)throws Exception;
+	public Map<String,String>cargarDatosGrupoLinea(Map<String,String>params)throws Exception;
+	public List<Map<String,String>>cargarTvalogarsGrupo(Map<String,String>params)throws Exception;
+	public List<Map<String,String>>cargarTarifasPorEdad(Map<String,String>params)throws Exception;
+	public List<Map<String,String>>cargarTarifasPorCobertura(Map<String,String>params)throws Exception;
 }
