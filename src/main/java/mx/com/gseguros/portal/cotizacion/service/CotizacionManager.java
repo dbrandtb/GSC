b3f4f3494f4b3580cd35309b42acf42ba1266075
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2014-07-18 17:52:25 +0000

    SE AGREGAN METODOS. NO ESTAN EN BD PRODUCCION. HACE FALTA SINCRONIZAR 'MODIFICACIONES BD'

--- src/main/java/mx/com/gseguros/portal/cotizacion/service/CotizacionManager.java
+++ src/main/java/mx/com/gseguros/portal/cotizacion/service/CotizacionManager.java
@@ -1,5 +1,8 @@
 package mx.com.gseguros.portal.cotizacion.service;
 
+import java.util.List;
+import java.util.Map;
+
 
 public interface CotizacionManager
 {
@@ -30,7 +33,8 @@ public interface CotizacionManager
 			,String otvalor10
 			,String otvalor11
 			,String otvalor12
-			,String otvalor13)throws Exception;
+			,String otvalor13
+			,String otvalor16)throws Exception;
 	
 	public void movimientoMpoligarGrupo(
 			String  cdunieco
@@ -44,5 +48,53 @@ public interface CotizacionManager
 			,String status
 			,String cdmoneda
 			,String accion)throws Exception;
+	
+	public Map<String,String> cargarDatosCotizacionGrupo(
+			String cdunieco
+			,String cdramo
+			,String cdtipsit
+			,String estado
+			,String nmpoliza
+			,String ntramite) throws Exception;
+	
+	public List<Map<String,String>>cargarGruposCotizacion(
+			String cdunieco
+			,String cdramo
+			,String estado
+			,String nmpoliza)throws Exception;
+	
+	public Map<String,String>cargarDatosGrupoLinea(
+			String cdunieco
+			,String cdramo
+			,String estado
+			,String nmpoliza
+			,String cdgrupo)throws Exception;
+	
+	public List<Map<String,String>>cargarTvalogarsGrupo(
+			String cdunieco
+			,String cdramo
+			,String estado
+			,String nmpoliza
+			,String cdgrupo)throws Exception;
+	
+	public List<Map<String,String>>cargarTarifasPorEdad(
+			String cdunieco
+			,String cdramo
+			,String estado
+			,String nmpoliza
+			,String nmsuplem
+			,String cdplan
+			,String cdgrupo
+			,String cdperpag)throws Exception;
+	
+	public List<Map<String,String>>cargarTarifasPorCobertura(
+			String cdunieco
+			,String cdramo
+			,String estado
+			,String nmpoliza
+			,String nmsuplem
+			,String cdplan
+			,String cdgrupo
+			,String cdperpag)throws Exception;
 	
 }
