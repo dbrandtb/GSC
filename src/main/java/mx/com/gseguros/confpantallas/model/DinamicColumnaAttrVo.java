ddbf2e575e8701fa7f997996c913019d47496411
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2014-09-11 23:23:45 +0000

    Recargar el Grid

---
+++ src/main/java/mx/com/gseguros/confpantallas/model/DinamicColumnaAttrVo.java
@@ -0,0 +1,45 @@
+package mx.com.gseguros.confpantallas.model;
+
+public class DinamicColumnaAttrVo implements java.io.Serializable  {
+	private static final long serialVersionUID = 1L;
+	
+	private String texto;
+	private Integer width;
+	private String tipoG;
+	private String dataIndex;
+	private String name;
+	
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Integer getWidth() {
+		return width;
+	}
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+	public String getTipoG() {
+		return tipoG;
+	}
+	public void setTipoG(String tipoG) {
+		this.tipoG = tipoG;
+	}
+	public String getDataIndex() {
+		return dataIndex;
+	}
+	public void setDataIndex(String dataIndex) {
+		this.dataIndex = dataIndex;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
