36cc2a3f99f1605381482cf9729c7a6d5fafcb66
HectorLT <HectorLT@68539698-d818-11e6-ad83-4fbd610fe048>
2014-09-24 00:50:00 +0000

    Se agrega el Tipo de Situacion RECIPERA_INDIVIDUAL y GASTOS_MEDICOS_INDIVIDUAL para abrir los WS de cliente Emision y DXN

--- trunk/src/main/java/mx/com/gseguros/portal/general/util/TipoSituacion.java
+++ trunk/src/main/java/mx/com/gseguros/portal/general/util/TipoSituacion.java
@@ -2,14 +2,16 @@ package mx.com.gseguros.portal.general.util;
 
 public enum TipoSituacion {
 	
-	AUTOS_FRONTERIZOS      ("AF"),
-	AUTOS_PICK_UP          ("PU"),
-	MULTISALUD             ("MS"),
-	MULTISALUD_COLECTIVO   ("MSC"),
-	SALUD_NOMINA           ("SN"),
-	SALUD_VITAL            ("SL"),
-	SERVICIO_PUBLICO_AUTO  ("AT"),
-	SERVICIO_PUBLICO_MICRO ("MC")
+	AUTOS_FRONTERIZOS         ("AF"),
+	AUTOS_PICK_UP             ("PU"),
+	MULTISALUD                ("MS"),
+	MULTISALUD_COLECTIVO      ("MSC"),
+	SALUD_NOMINA              ("SN"),
+	SALUD_VITAL               ("SL"),
+	SERVICIO_PUBLICO_AUTO     ("AT"),
+	SERVICIO_PUBLICO_MICRO    ("MC"),
+	RECIPERA_INDIVIDUAL       ("RI"),
+	GASTOS_MEDICOS_INDIVIDUAL ("GMI")
 	;
 
 	private String cdtipsit;
