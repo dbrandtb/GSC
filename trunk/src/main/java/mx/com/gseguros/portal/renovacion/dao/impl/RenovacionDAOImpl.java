d8b0a438bfa9853cc0996adb3f94f2348e4c1c14
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2014-09-25 20:20:13 +0000

    SEGUNDO PASO DE RENOVACION.

--- trunk/src/main/java/mx/com/gseguros/portal/renovacion/dao/impl/RenovacionDAOImpl.java
+++ trunk/src/main/java/mx/com/gseguros/portal/renovacion/dao/impl/RenovacionDAOImpl.java
@@ -1,6 +1,7 @@
 package mx.com.gseguros.portal.renovacion.dao.impl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,11 +20,13 @@ import org.springframework.jdbc.object.StoredProcedure;
 public class RenovacionDAOImpl extends AbstractManagerDAO implements RenovacionDAO
 {
 	@Override
-	public List<Map<String,String>>buscarPolizasRenovables(String anio,String mes)throws Exception
+	public List<Map<String,String>>buscarPolizasRenovables(String cdunieco,String cdramo,String anio,String mes)throws Exception
 	{
 		Map<String,String>params=new HashMap<String,String>();
-		params.put("anio" , anio);
-		params.put("mes"  , mes);
+		params.put("cdunieco" , cdunieco);
+		params.put("cdramo"   , cdramo);
+		params.put("anio"     , anio);
+		params.put("mes"      , mes);
 		Map<String,Object>procedureResult=ejecutaSP(new BuscarPolizasRenovables(getDataSource()),params);
 		List<Map<String,String>>lista=(List<Map<String,String>>)procedureResult.get("pv_registro_o");
 		if(lista==null)
@@ -37,18 +40,76 @@ public class RenovacionDAOImpl extends AbstractManagerDAO implements RenovacionD
 	{
 		protected BuscarPolizasRenovables(DataSource dataSource)
 		{
-			super(dataSource, "PKG");
-			declareParameter(new SqlParameter("anio" , OracleTypes.VARCHAR));
-			declareParameter(new SqlParameter("mes"  , OracleTypes.VARCHAR));
+			super(dataSource, "PKG_CONSULTA.P_GET_POLIZAS_RENOVABLES");
+			declareParameter(new SqlParameter("cdunieco" , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("cdramo"   , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("anio"     , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("mes"      , OracleTypes.VARCHAR));
 			String[] columnas=new String[]{
 					"cdunieco"
 					,"cdramo"
 					,"estado"
 					,"nmpoliza"
+					,"feefecto"
+					,"feproren"
+					,"cliente"
+					,"anio"
+					,"mes"
+					,"cdtipopc"
 					};
 			declareParameter(new SqlOutParameter("pv_registro_o" , OracleTypes.CURSOR , new GenericMapper(columnas)));
 			declareParameter(new SqlOutParameter("pv_msg_id_o"   , OracleTypes.NUMERIC));
 			declareParameter(new SqlOutParameter("pv_title_o"    , OracleTypes.VARCHAR));
+			compile();
+		}
+	}
+	
+	@Override
+	public void marcarPoliza(String anio
+			,String mes
+			,String cdtipopc
+			,String cdtipacc
+			,String cdunieco
+			,String cdramo
+			,String nmpoliza
+			,Date   feemisio
+			,String swrenova
+			,String swaproba
+			,String nmsituac)throws Exception
+	{
+		Map<String,Object>params=new HashMap<String,Object>();
+		params.put("anio"     , anio);
+		params.put("mes"      , mes);
+		params.put("cdtipopc" , cdtipopc);
+		params.put("cdtipacc" , cdtipacc);
+		params.put("cdunieco" , cdunieco);
+		params.put("cdramo"   , cdramo);
+		params.put("nmpoliza" , nmpoliza);
+		params.put("feemisio" , feemisio);
+		params.put("swrenova" , swrenova);
+		params.put("swaproba" , swaproba);
+		params.put("nmsituac" , nmsituac);
+		ejecutaSP(new MarcarPoliza(getDataSource()),params);
+	}
+	
+	protected class MarcarPoliza extends StoredProcedure
+	{
+		protected MarcarPoliza(DataSource dataSource)
+		{
+			super(dataSource, "PKG_RENOVA.P_MARCAR_POLIZA");
+			declareParameter(new SqlParameter("anio"     , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("mes"      , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("cdtipopc" , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("cdtipacc" , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("cdunieco" , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("cdramo"   , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("nmpoliza" , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("feemisio" , OracleTypes.DATE));
+			declareParameter(new SqlParameter("swrenova" , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("swaproba" , OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("nmsituac" , OracleTypes.VARCHAR));
+			declareParameter(new SqlOutParameter("pv_msg_id_o" , OracleTypes.NUMERIC));
+			declareParameter(new SqlOutParameter("pv_title_o"  , OracleTypes.VARCHAR));
 			compile();
 		}
 	}
--- trunk/src/main/java/mx/com/gseguros/portal/renovacion/service/impl/RenovacionManagerImpl.java
+++ trunk/src/main/java/mx/com/gseguros/portal/renovacion/service/impl/RenovacionManagerImpl.java
@@ -1,5 +1,7 @@
 package mx.com.gseguros.portal.renovacion.service.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,6 +9,7 @@ import java.util.Map;
 import mx.com.gseguros.portal.cotizacion.model.Item;
 import mx.com.gseguros.portal.cotizacion.model.ManagerRespuestaImapVO;
 import mx.com.gseguros.portal.cotizacion.model.ManagerRespuestaSlistVO;
+import mx.com.gseguros.portal.cotizacion.model.ManagerRespuestaVoidVO;
 import mx.com.gseguros.portal.general.dao.PantallasDAO;
 import mx.com.gseguros.portal.general.model.ComponenteVO;
 import mx.com.gseguros.portal.general.util.GeneradorCampos;
@@ -18,7 +21,8 @@ import org.apache.struts2.ServletActionContext;
 
 public class RenovacionManagerImpl implements RenovacionManager
 {
-	private static final Logger logger = Logger.getLogger(RenovacionManagerImpl.class);
+	private static final Logger           logger       = Logger.getLogger(RenovacionManagerImpl.class);
+	private static final SimpleDateFormat renderFechas = new SimpleDateFormat("dd/MM/yyyy"); 
 	
 	//Dependencias inyectadas
 	private RenovacionDAO renovacionDAO;
@@ -46,9 +50,17 @@ public class RenovacionManagerImpl implements RenovacionManager
 			gc.generaComponentes(componentesBusqueda, true, false, true, false, false, false);
 			
 			Map<String,Item> imap = new HashMap<String,Item>();
+			resp.setImap(imap);
 			
 			imap.put("busquedaItems" , gc.getItems());
-			resp.setImap(imap);
+			
+			List<ComponenteVO>componentesGrid=pantallasDAO.obtenerComponentes(
+					null,null,null,null,null,cdsisrol,"PANTALLA_RENOVACION","GRID",null);
+			
+			gc.generaComponentes(componentesGrid, true, true, false, true, true, false);
+			
+			imap.put("gridFields"  , gc.getFields());
+			imap.put("gridColumns" , gc.getColumns());
 		}
 		catch(Exception ex)
 		{
@@ -74,14 +86,16 @@ public class RenovacionManagerImpl implements RenovacionManager
 	}
 	
 	@Override
-	public ManagerRespuestaSlistVO buscarPolizasRenovables(String anio,String mes)
+	public ManagerRespuestaSlistVO buscarPolizasRenovables(String cdunieco,String cdramo,String anio,String mes)
 	{
 		logger.info(
 				new StringBuilder()
 				.append("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 				.append("\n@@@@@@ buscarPolizasRenovables @@@@@@")
-				.append("\n@@@@@@ anio=").append(anio)
-				.append("\n@@@@@@ mes=").append(mes)
+				.append("\n@@@@@@ cdunieco=").append(cdunieco)
+				.append("\n@@@@@@ cdramo=")  .append(cdramo)
+				.append("\n@@@@@@ anio=")    .append(anio)
+				.append("\n@@@@@@ mes=")     .append(mes)
 				.toString()
 				);
 		ManagerRespuestaSlistVO resp = new ManagerRespuestaSlistVO(true);
@@ -89,7 +103,7 @@ public class RenovacionManagerImpl implements RenovacionManager
 		//obtener polizas
 		try
 		{
-			List<Map<String,String>>listaPolizasRenovables = renovacionDAO.buscarPolizasRenovables(anio,mes);
+			List<Map<String,String>>listaPolizasRenovables = renovacionDAO.buscarPolizasRenovables(cdunieco,cdramo,anio,mes);
 			resp.setSlist(listaPolizasRenovables);
 		}
 		catch(Exception ex)
@@ -106,6 +120,63 @@ public class RenovacionManagerImpl implements RenovacionManager
 				.append("\n@@@@@@ ").append(resp)
 				.append("\n@@@@@@ buscarPolizasRenovables @@@@@@")
 				.append("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+				.toString()
+				);
+		return resp;
+	}
+	
+	@Override
+	public ManagerRespuestaVoidVO renovarPolizas(List<Map<String,String>>polizas)
+	{
+		logger.info(
+				new StringBuilder()
+				.append("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+				.append("\n@@@@@@ renovarPolizas @@@@@@")
+				.append("\n@@@@@@ polizas=").append(polizas)
+				.toString()
+				);
+		
+		ManagerRespuestaVoidVO resp=new ManagerRespuestaVoidVO(true);
+		
+		//marcar polizas
+		try
+		{
+			for(Map<String,String>poliza:polizas)
+			{
+				renovacionDAO.marcarPoliza(
+						poliza.get("anio")
+						,poliza.get("mes")
+						,poliza.get("cdtipopc")
+						,"1"//cdtipacc
+						,poliza.get("cdunieco")
+						,poliza.get("cdramo")
+						,poliza.get("nmpoliza")
+						,new Date()//feemisio
+						,"S"//swrenova
+						,"S"//swaproba
+						,"1"//nmsituac
+						);
+			}
+		}
+		catch(Exception ex)
+		{
+			long timestamp = System.currentTimeMillis();
+			resp.setExito(false);
+			resp.setRespuesta(new StringBuilder("Error al marcar polizas #").append(timestamp).toString());
+			resp.setRespuestaOculta(ex.getMessage());
+		}
+		
+		if(resp.isExito())
+		{
+			resp.setRespuesta(new StringBuilder("Se renovaron ").append(polizas.size()).append(" polizas").toString());
+			resp.setRespuestaOculta("Todo OK");
+		}
+		
+		logger.info(
+				new StringBuilder()
+				.append("\n@@@@@@ ").append(resp)
+				.append("\n@@@@@@ renovarPolizas @@@@@@")
+				.append("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 				.toString()
 				);
 		return resp;
