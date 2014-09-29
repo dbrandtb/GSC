0f4b89f51002228edaa00e625dc0583cd6bf734d
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2014-09-29 17:22:17 +0000

    Se agregan métodos para la obtención de datos de los agentes y recibos.

--- trunk/src/main/java/mx/com/gseguros/portal/consultas/dao/IConsultasPolizaDAO.java
+++ trunk/src/main/java/mx/com/gseguros/portal/consultas/dao/IConsultasPolizaDAO.java
@@ -1,14 +1,17 @@
 package mx.com.gseguros.portal.consultas.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosAseguradoVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosPolizaVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosSuplementoVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaPolizaAseguradoVO;
+import mx.com.gseguros.portal.consultas.model.ConsultaReciboAgenteVO;
 import mx.com.gseguros.portal.consultas.model.CopagoVO;
+import mx.com.gseguros.portal.general.model.AgenteVO;
+import mx.com.gseguros.portal.general.model.DetalleReciboVO;
 import mx.com.gseguros.portal.general.model.PolizaVO;
+import mx.com.gseguros.portal.general.model.ReciboVO;
 
 
 public interface IConsultasPolizaDAO {
@@ -78,6 +81,43 @@ public interface IConsultasPolizaDAO {
      * @throws Exception
      */
     public List<ConsultaDatosAseguradoVO> obtieneAsegurados(PolizaVO poliza) throws Exception;
+    
+    
+    /**
+     * Obtiene los recibos del agente asociados a una p&oacute;liza
+     * @param poliza
+     * @return
+     * @throws Exception
+     */
+    public List<ConsultaReciboAgenteVO> obtieneRecibosAgente(PolizaVO poliza) throws Exception;
+    
+    
+    /**
+     * Obtiene los datos de un recibo asociado a una p&oacute;liza
+     * @param poliza
+     * @param recibo
+     * @return
+     * @throws Exception
+     */
+    public List<DetalleReciboVO> obtieneDetalleRecibo(PolizaVO poliza, ReciboVO recibo) throws Exception;
+    
+    
+    /**
+     * Obtiene los agentes asociados a una p&oacute;liza
+     * @param poliza
+     * @return
+     * @throws Exception
+     */
+    public List<AgenteVO> obtieneAgentesPoliza(PolizaVO poliza) throws Exception;
+    
+    
+    /**
+     * Obtiene los recibos asociados a una p&oacute;liza
+     * @param poliza
+     * @return
+     * @throws Exception
+     */
+    public List<ConsultaReciboAgenteVO> obtieneRecibosPoliza(PolizaVO poliza) throws Exception;
     
 	
 }
--- trunk/src/main/java/mx/com/gseguros/portal/consultas/dao/impl/ConsultasPolizaDAOImpl.java
+++ trunk/src/main/java/mx/com/gseguros/portal/consultas/dao/impl/ConsultasPolizaDAOImpl.java
@@ -13,9 +13,13 @@ import mx.com.gseguros.portal.consultas.model.ConsultaDatosAseguradoVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosPolizaVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosSuplementoVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaPolizaAseguradoVO;
+import mx.com.gseguros.portal.consultas.model.ConsultaReciboAgenteVO;
 import mx.com.gseguros.portal.consultas.model.CopagoVO;
 import mx.com.gseguros.portal.dao.AbstractManagerDAO;
+import mx.com.gseguros.portal.general.model.AgenteVO;
+import mx.com.gseguros.portal.general.model.DetalleReciboVO;
 import mx.com.gseguros.portal.general.model.PolizaVO;
+import mx.com.gseguros.portal.general.model.ReciboVO;
 import mx.com.gseguros.utils.Utilerias;
 import oracle.jdbc.driver.OracleTypes;
 
@@ -178,6 +182,33 @@ public class ConsultasPolizaDAOImpl extends AbstractManagerDAO implements IConsu
 
 	@Override
 	public List<ConsultaDatosAseguradoVO> obtieneAsegurados(PolizaVO poliza)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ConsultaReciboAgenteVO> obtieneRecibosAgente(PolizaVO poliza)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DetalleReciboVO> obtieneDetalleRecibo(PolizaVO poliza,
+			ReciboVO recibo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AgenteVO> obtieneAgentesPoliza(PolizaVO poliza) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ConsultaReciboAgenteVO> obtieneRecibosPoliza(PolizaVO poliza)
 			throws Exception {
 		// TODO Auto-generated method stub
 		return null;
