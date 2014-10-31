31249c5f527dbf9a04eca86c56ef5d20a5c25ff6
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2014-10-31 17:08:38 +0000

    Se agregaron atributos a ClausulaVO, modificandolos también en la vista.

--- trunk/src/main/java/mx/com/gseguros/portal/general/model/ClausulaVO.java
+++ trunk/src/main/java/mx/com/gseguros/portal/general/model/ClausulaVO.java
@@ -30,22 +30,56 @@ public class ClausulaVO implements Serializable {
 	private String status;
 	
 	/**
-	 * Contenido de la clausula
+	 * Contenido de la clausula modificado 
 	 */
-	private String contenidoClausula;
+	private String linea_usuario;
 	
+	/**
+	 * Contenido original de la clausula
+	 */
+	private String linea_general;
 	
-	//private String swmodi; 
+	/**
+	 * Indica si fue modificado el contenido original de la clausula
+	 */
+	private String swmodi;
+	
+	/**
+	 * Unidad economica asociada a la clausula
+	 */
+	private String cdunieco;
+	
+	/**
+	 * Ramo asociado a la clausula
+	 */
+	private String cdramo;
+	
+	/**
+	 * Estado asociado a la clausula
+	 */
+	private String estado;
+	
+	/**
+	 * Numero de poliza asociado a la clausula
+	 */
+	private String nmpoliza;
+	
+	/**
+	 * Numero de situacion asociado a la clausula
+	 */
+	private String nmsituac;
+	
+	/**
+	 * Numero de suplemento asociado a la clausula
+	 */
+	private String nmsuplem;
 	
 	
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
 	}
-	
-	
-	// Getters and setters:
-	
+
 	public String getCdclausu() {
 		return cdclausu;
 	}
@@ -78,12 +112,76 @@ public class ClausulaVO implements Serializable {
 		this.status = status;
 	}
 
-	public String getContenidoClausula() {
-		return contenidoClausula;
+	public String getLinea_usuario() {
+		return linea_usuario;
 	}
 
-	public void setContenidoClausula(String contenidoClausula) {
-		this.contenidoClausula = contenidoClausula;
+	public void setLinea_usuario(String linea_usuario) {
+		this.linea_usuario = linea_usuario;
+	}
+
+	public String getLinea_general() {
+		return linea_general;
+	}
+
+	public void setLinea_general(String linea_general) {
+		this.linea_general = linea_general;
+	}
+
+	public String getSwmodi() {
+		return swmodi;
+	}
+
+	public void setSwmodi(String swmodi) {
+		this.swmodi = swmodi;
+	}
+
+	public String getCdunieco() {
+		return cdunieco;
+	}
+
+	public void setCdunieco(String cdunieco) {
+		this.cdunieco = cdunieco;
+	}
+
+	public String getCdramo() {
+		return cdramo;
+	}
+
+	public void setCdramo(String cdramo) {
+		this.cdramo = cdramo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getNmpoliza() {
+		return nmpoliza;
+	}
+
+	public void setNmpoliza(String nmpoliza) {
+		this.nmpoliza = nmpoliza;
+	}
+
+	public String getNmsituac() {
+		return nmsituac;
+	}
+
+	public void setNmsituac(String nmsituac) {
+		this.nmsituac = nmsituac;
+	}
+
+	public String getNmsuplem() {
+		return nmsuplem;
+	}
+
+	public void setNmsuplem(String nmsuplem) {
+		this.nmsuplem = nmsuplem;
 	}
 
 }
