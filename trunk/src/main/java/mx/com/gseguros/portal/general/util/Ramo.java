3a8b759eafbf00bb01ffaad6e6628086d4417be1
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2014-11-21 03:19:00 +0000

    Se agregan elementos

--- trunk/src/main/java/mx/com/gseguros/portal/general/util/Ramo.java
+++ trunk/src/main/java/mx/com/gseguros/portal/general/util/Ramo.java
@@ -5,8 +5,10 @@ public enum Ramo {
 	AUTOS_FRONTERIZOS("16"),
 	AUTOS_RESIDENTES ("5"),
 	MULTISALUD       ("4"),
+	RECUPERA         ("1"),
 	SALUD_VITAL      ("2"),
-	SERVICIO_PUBLICO ("6");
+	SERVICIO_PUBLICO ("6"),
+	GASTOS_MEDICOS_MAYORES("7");
 
 	private String cdramo;
 
