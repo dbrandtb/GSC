24f4749e93a76b2a4319b7650b14563fdaeefc3f
mavieyrar <mavieyrar@68539698-d818-11e6-ad83-4fbd610fe048>
2014-12-11 23:50:48 +0000

    Archivos requeridos para Consulta de Asegurados de los sistemas ICE y SISA.

--- trunk/src/main/java/mx/com/gseguros/portal/consultas/service/ConsultasAseguradoManager.java
+++ trunk/src/main/java/mx/com/gseguros/portal/consultas/service/ConsultasAseguradoManager.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 import mx.com.gseguros.portal.consultas.model.AseguradoDetalleVO;
 import mx.com.gseguros.portal.consultas.model.AseguradoVO;
-import mx.com.gseguros.portal.consultas.model.CoberturasBasicasVO;
+import mx.com.gseguros.portal.consultas.model.CoberturaBasicaVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosComplementariosVO;
-import mx.com.gseguros.portal.consultas.model.ConsultaDatosContratanteVO;
+import mx.com.gseguros.portal.consultas.model.ContratanteVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosGeneralesPolizaVO;
-import mx.com.gseguros.portal.consultas.model.ConsultaDatosHistoricoVO;
-import mx.com.gseguros.portal.consultas.model.ConsultaDatosPlanVO;
+import mx.com.gseguros.portal.consultas.model.HistoricoVO;
+import mx.com.gseguros.portal.consultas.model.PlanVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaDatosTitularVO;
-import mx.com.gseguros.portal.consultas.model.ConsultaPeriodosVigenciaVO;
+import mx.com.gseguros.portal.consultas.model.PeriodoVigenciaVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaPolizaActualVO;
-import mx.com.gseguros.portal.consultas.model.ConsultaPolizaAseguradoVO;
+import mx.com.gseguros.portal.consultas.model.PolizaAseguradoVO;
 import mx.com.gseguros.portal.consultas.model.ConsultaResultadosAseguradoVO;
 import mx.com.gseguros.portal.consultas.model.CopagoVO;
 import mx.com.gseguros.portal.consultas.model.EndosoVO;
@@ -25,17 +25,17 @@ public interface ConsultasAseguradoManager {
 	
 	public List<ConsultaResultadosAseguradoVO> obtieneResultadosAsegurado(String rfc, String cdperson, String nombre) throws Exception;
 
-	public List<ConsultaPolizaActualVO> obtienePolizaActual(ConsultaPolizaAseguradoVO polizaAsegurado) throws Exception;
+	public List<ConsultaPolizaActualVO> obtienePolizaActual(PolizaAseguradoVO polizaAsegurado) throws Exception;
 	
 	public List<ConsultaDatosComplementariosVO> obtieneDatosComplementarios(PolizaVO poliza, AseguradoVO asegurado) throws Exception;
 	
-	public List<ConsultaDatosGeneralesPolizaVO> obtieneDatosPoliza(ConsultaPolizaAseguradoVO polizaAsegurado) throws Exception;
+	public List<ConsultaDatosGeneralesPolizaVO> obtieneDatosPoliza(PolizaAseguradoVO polizaAsegurado) throws Exception;
 	
 	public List<AseguradoDetalleVO> obtieneAseguradoDetalle(PolizaVO poliza, AseguradoVO asegurado) throws Exception;
 	
 	public List<ConsultaDatosTitularVO> obtieneDatosTitular(PolizaVO poliza, AseguradoVO asegurado) throws Exception;
 	
-	public List<ConsultaDatosContratanteVO> obtieneDatosContratante(PolizaVO poliza) throws Exception;
+	public List<ContratanteVO> obtieneDatosContratante(PolizaVO poliza) throws Exception;
 	
 	public List<AseguradoVO> obtieneAsegurados(PolizaVO poliza, AseguradoVO asegurado) throws Exception;
 	
@@ -43,18 +43,18 @@ public interface ConsultasAseguradoManager {
 	
 	public List<EnfermedadVO> obtieneEnfermedades(PolizaVO poliza, AseguradoVO asegurado) throws Exception;
 	
-	public List<ConsultaDatosPlanVO> obtieneDatosPlan(PolizaVO poliza) throws Exception;
+	public List<PlanVO> obtieneDatosPlan(PolizaVO poliza) throws Exception;
 	
 	public List<CopagoVO> obtieneCopagosPoliza(PolizaVO poliza) throws Exception;
 	
-	public List<CoberturasBasicasVO> obtieneCoberturasPoliza(PolizaVO poliza) throws Exception;
+	public List<CoberturaBasicaVO> obtieneCoberturasPoliza(PolizaVO poliza) throws Exception;
 	
-	public List<CoberturasBasicasVO> obtieneCoberturasBasicas(PolizaVO poliza) throws Exception;
+	public List<CoberturaBasicaVO> obtieneCoberturasBasicas(PolizaVO poliza) throws Exception;
 	
-	public List<ConsultaDatosHistoricoVO> obtieneHistoricoAsegurado(ConsultaPolizaAseguradoVO polizaAsegurado, AseguradoVO asegurado) throws Exception;
+	public List<HistoricoVO> obtieneHistoricoAsegurado(PolizaAseguradoVO polizaAsegurado, AseguradoVO asegurado) throws Exception;
 	
 	public List<HistoricoFarmaciaVO> obtieneHistoricoFarmacia(PolizaVO poliza, AseguradoVO asegurado) throws Exception;
 	
-	public List<ConsultaPeriodosVigenciaVO> obtienePeriodosVigencia(PolizaVO poliza, AseguradoVO asegurado) throws Exception;	
+	public List<PeriodoVigenciaVO> obtienePeriodosVigencia(PolizaVO poliza, AseguradoVO asegurado) throws Exception;	
 	
 }
