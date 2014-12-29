d871eb87e8636d3e5e762f04077e3fecf68ae1c9
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2014-12-29 20:51:29 +0000

    Se agrega el metodo carga masiva para hacer un bulk insert en tablas de 1 y 5 claves.

--- trunk/src/main/java/mx/com/gseguros/wizard/dao/TablasApoyoDAO.java
+++ trunk/src/main/java/mx/com/gseguros/wizard/dao/TablasApoyoDAO.java
@@ -6,9 +6,51 @@ import java.util.Map;
 
 public interface TablasApoyoDAO {
 	
+	/**
+	 * 
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
 	public List<Map<String, String>> obtieneValoresTablaApoyo5claves(Map<String,String> params) throws Exception;
+
+	
+	/**
+	 * 
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
 	public List<Map<String, String>> obtieneValoresTablaApoyo1clave(Map<String,String> params) throws Exception;
+	
+	
+	/**
+	 * 
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
 	public String guardaValoresTablaApoyo(Map<String,String> params) throws Exception;
+	
+	
+	/**
+	 * 
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
 	public String guardaValoresTablaApoyo1Clave(Map<String,String> params) throws Exception;
+	
+	
+	/**
+	 * 
+	 * @param nmtabla
+	 * @param tipoTabla
+	 * @param nombreArchivo
+	 * @param separador
+	 * @return
+	 * @throws Exception
+	 */
+	public String cargaMasiva(Integer nmtabla, Integer tipoTabla, String nombreArchivo, String separador) throws Exception; 
 	
 }
