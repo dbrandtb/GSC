251ece3b4a9022d3e5d7533447ae44451e7935ad
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2015-03-24 03:15:00 +0000

    PARA ENDOSO DE CAMBIO DE NOMBRE DE CLIENTE

--- src/main/java/mx/com/gseguros/portal/consultas/dao/impl/ConsultasDAOImpl.java
+++ src/main/java/mx/com/gseguros/portal/consultas/dao/impl/ConsultasDAOImpl.java
@@ -1026,7 +1026,7 @@ public class ConsultasDAOImpl extends AbstractManagerDAO implements ConsultasDAO
     		declareParameter(new SqlParameter("cdrol" , OracleTypes.VARCHAR));
     		declareParameter(new SqlParameter("cdperson" , OracleTypes.VARCHAR));
     		String[] cols=new String[]{
-    				"NOMBRE","CDRFC","CDPERSON","CDIDEPER","CDIDEEXT", "NMSITUAC", "CDROL", "STATUS", "NMORDDOM", "SWRECLAM"
+    				"NOMBRE","CDRFC","CDPERSON","CDIDEPER","CDIDEEXT", "NMSITUAC", "CDROL", "STATUS", "NMORDDOM", "SWRECLAM", "OTFISJUR"
     		};
     		declareParameter(new SqlOutParameter("pv_registro_o" , OracleTypes.CURSOR, new GenericMapper(cols)));
     		declareParameter(new SqlOutParameter("pv_msg_id_o"   , OracleTypes.NUMERIC));
