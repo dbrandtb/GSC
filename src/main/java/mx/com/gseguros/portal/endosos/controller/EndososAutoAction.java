b6c82a0e5f20785d8e2605a1a3f765dc8bbd582d
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2015-04-08 22:59:31 +0000

    Endosos Auto - Agregar Asegurados Alterno y Fechas

--- src/main/java/mx/com/gseguros/portal/endosos/controller/EndososAutoAction.java
+++ src/main/java/mx/com/gseguros/portal/endosos/controller/EndososAutoAction.java
@@ -592,6 +592,7 @@ public class EndososAutoAction extends PrincipalCoreAction
 			//1.Obtenemos los n�meros de d�as Maximo y Minimo
 			List<Map<String,String>> retroactividad = endososAutoManager.obtenerRetroactividad(cdsisrol,cdramo,cdtipsup, fechaProceso);
 			endososAutoManager.validarEndosoAnterior(cdunieco, cdramo, estado, nmpoliza, cdtipsup);
+			endososAutoManager.validarEndosoPagados(cdunieco, cdramo, estado, nmpoliza);
 			success   = true;
 			
 			smap1.put("pv_cdunieco", smap1.get("CDUNIECO"));
