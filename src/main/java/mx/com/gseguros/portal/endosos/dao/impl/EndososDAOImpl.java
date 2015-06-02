33cf6909a0a9bc6d35a86ff40135ad0163ea06e2
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2015-06-02 16:09:23 +0000

    Se agrega el parametro pv_cdsisrol_i a PKG_COTIZA.P_GET_COBERTURAS_DISP para mostrar las coberturas opcionales y no opcionales cuando el rol es Suscriptor.

--- src/main/java/mx/com/gseguros/portal/endosos/dao/impl/EndososDAOImpl.java
+++ src/main/java/mx/com/gseguros/portal/endosos/dao/impl/EndososDAOImpl.java
@@ -323,6 +323,7 @@ public class EndososDAOImpl extends AbstractManagerDAO implements EndososDAO
 			declareParameter(new SqlParameter("pv_estado_i",      OracleTypes.VARCHAR));
 			declareParameter(new SqlParameter("pv_nmpoliza_i",    OracleTypes.VARCHAR));
 			declareParameter(new SqlParameter("pv_nmsituac_i",    OracleTypes.VARCHAR));
+			declareParameter(new SqlParameter("pv_cdsisrol_i",    OracleTypes.VARCHAR));
 			declareParameter(new SqlOutParameter("pv_registro_o",   OracleTypes.CURSOR, new GenericMapper(columnas)));
 			declareParameter(new SqlOutParameter("pv_msg_id_o",     OracleTypes.NUMERIC));
 			declareParameter(new SqlOutParameter("pv_title_o",      OracleTypes.VARCHAR));
