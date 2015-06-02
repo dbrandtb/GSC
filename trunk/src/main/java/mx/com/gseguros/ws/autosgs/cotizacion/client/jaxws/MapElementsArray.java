2fdc95a424e7ecf7adef46730cfff0086bb111c4
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2015-06-02 23:20:16 +0000

    UTILS EN LUGAR DE UTILERIAS

--- trunk/src/main/java/mx/com/gseguros/ws/autosgs/cotizacion/client/jaxws/MapElementsArray.java
+++ trunk/src/main/java/mx/com/gseguros/ws/autosgs/cotizacion/client/jaxws/MapElementsArray.java
@@ -3,6 +3,7 @@ package mx.com.gseguros.ws.autosgs.cotizacion.client.jaxws;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
