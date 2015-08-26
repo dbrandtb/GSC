dbc03457a1f92fb8becd6a4f892b5e411fba4e08
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2015-08-26 01:09:57 +0000

    Cambios para la Emisión Directa.

--- src/main/java/mx/com/gseguros/portal/general/service/ProcesoEmisionManager.java
+++ src/main/java/mx/com/gseguros/portal/general/service/ProcesoEmisionManager.java
@@ -1,11 +1,11 @@
 package mx.com.gseguros.portal.general.service;
 
-import mx.com.gseguros.portal.general.model.RespuestaEmisionIndividualVO;
+import java.util.Map;
 
 
 public interface ProcesoEmisionManager {
 	
-	public RespuestaEmisionIndividualVO emitir(String cdunieco, String cdramo, String estado, String nmpoliza, 
+	public Map<String, String> emitir(String cdunieco, String cdramo, String estado, String nmpoliza, 
 			String cdtipsit, String ntramite, String cdusuari, String cdsisrol, String cdelemento,
 			boolean esFlotilla, String tipoGrupoInciso) throws Exception;
 	
