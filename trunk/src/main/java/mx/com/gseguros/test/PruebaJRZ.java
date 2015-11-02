8fd57a04c522f17b383f2aba39045cf786f14386
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2015-11-02 20:58:55 +0000

    Se eliminan clases de prueba.

--- trunk/src/main/java/mx/com/gseguros/test/PruebaJRZ.java
+++
@@ -1,6 +0,0 @@
-package mx.com.gseguros.test;
-
-public class PruebaJRZ {
-	//prueba johan nueva clase
-
-}
--- trunk/src/main/java/mx/com/gseguros/test/PruebaSLG.java
+++
@@ -1,7 +0,0 @@
-package mx.com.gseguros.test;
-
-public class PruebaSLG {
-	
-//	PRUEBA 1
-
-}
