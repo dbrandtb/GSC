5ed8a803371a24c536f6015663267729ed65a743
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2015-12-03 22:32:10 +0000

    CATALOGO 1

--- src/main/java/mx/com/gseguros/mesacontrol/dao/impl/FlujoMesaControlDAOImpl.java
+++ src/main/java/mx/com/gseguros/mesacontrol/dao/impl/FlujoMesaControlDAOImpl.java
@@ -1,15 +1,49 @@
 package mx.com.gseguros.mesacontrol.dao.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import mx.com.gseguros.mesacontrol.dao.FlujoMesaControlDAO;
 import mx.com.gseguros.portal.dao.AbstractManagerDAO;
+import mx.com.gseguros.portal.dao.impl.GenericMapper;
+import oracle.jdbc.driver.OracleTypes;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.object.StoredProcedure;
 
 public class FlujoMesaControlDAOImpl extends AbstractManagerDAO implements FlujoMesaControlDAO {
 	
 	private final static Logger logger = LoggerFactory.getLogger(FlujoMesaControlDAOImpl.class);
 	
+	@Override
+	public List<Map<String,String>> recuperaTtiptramc() throws Exception
+	{
+		Map<String,Object>       procRes = ejecutaSP(new RecuperaTtiptramcSP(getDataSource()),new HashMap<String,String>());
+		List<Map<String,String>> lista   = (List<Map<String,String>>)procRes.get("pv_registro_o");
+		if(lista==null)
+		{
+			lista = new ArrayList<Map<String,String>>();
+		}
+		return lista;
+	}
 	
+	protected class RecuperaTtiptramcSP extends StoredProcedure
+	{
+		protected RecuperaTtiptramcSP(DataSource dataSource)
+		{
+			super(dataSource,"PKG_MESACONTROL.P_GET_TTIPTRAMC");
+			String[] cols=new String[]{ "CDTIPTRA" , "DSTIPTRA" };
+			declareParameter(new SqlOutParameter("pv_registro_o" , OracleTypes.CURSOR, new GenericMapper(cols)));
+			declareParameter(new SqlOutParameter("pv_msg_id_o"   , OracleTypes.NUMERIC));
+			declareParameter(new SqlOutParameter("pv_title_o"    , OracleTypes.VARCHAR));
+			compile();
+		}
+	}
 
 }
