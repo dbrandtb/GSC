9ed0dd75388bfa2afa2715be825e69c84d269b86
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2015-12-03 22:13:53 +0000

    Se agrega DAO para operaciones del flujo de la Mesa de Control.

---
+++ src/main/java/mx/com/gseguros/mesacontrol/dao/FlujoMesaControlDAO.java
@@ -0,0 +1,5 @@
+package mx.com.gseguros.mesacontrol.dao;
+
+public interface FlujoMesaControlDAO {
+
+}
---
+++ src/main/java/mx/com/gseguros/mesacontrol/dao/impl/FlujoMesaControlDAOImpl.java
@@ -0,0 +1,15 @@
+package mx.com.gseguros.mesacontrol.dao.impl;
+
+import mx.com.gseguros.mesacontrol.dao.FlujoMesaControlDAO;
+import mx.com.gseguros.portal.dao.AbstractManagerDAO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlujoMesaControlDAOImpl extends AbstractManagerDAO implements FlujoMesaControlDAO {
+	
+	private final static Logger logger = LoggerFactory.getLogger(FlujoMesaControlDAOImpl.class);
+	
+	
+
+}
