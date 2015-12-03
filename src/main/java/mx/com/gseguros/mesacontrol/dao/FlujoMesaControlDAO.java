692ae8bce42ef3d739e03dba69843673aefcb7aa
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2015-12-03 22:31:59 +0000

    CATALOGO 1

--- src/main/java/mx/com/gseguros/mesacontrol/dao/FlujoMesaControlDAO.java
+++ src/main/java/mx/com/gseguros/mesacontrol/dao/FlujoMesaControlDAO.java
@@ -1,5 +1,10 @@
 package mx.com.gseguros.mesacontrol.dao;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FlujoMesaControlDAO {
 
+	public List<Map<String,String>> recuperaTtiptramc() throws Exception;
+	
 }
