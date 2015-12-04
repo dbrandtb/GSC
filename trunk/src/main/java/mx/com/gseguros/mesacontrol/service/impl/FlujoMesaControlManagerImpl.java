d7483c8ac8bc095f9b74eca355ee321f80f8869b
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2015-12-04 20:08:53 +0000

    TERCER AVANCE DE PANTALLA DE FLUJOS DE MESA DE CONTROL (PREVIO DEMO 1)

--- trunk/src/main/java/mx/com/gseguros/mesacontrol/service/impl/FlujoMesaControlManagerImpl.java
+++ trunk/src/main/java/mx/com/gseguros/mesacontrol/service/impl/FlujoMesaControlManagerImpl.java
@@ -1,13 +1,18 @@
 package mx.com.gseguros.mesacontrol.service.impl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import mx.com.gseguros.mesacontrol.dao.FlujoMesaControlDAO;
 import mx.com.gseguros.mesacontrol.service.FlujoMesaControlManager;
 import mx.com.gseguros.portal.cotizacion.model.Item;
+import mx.com.gseguros.portal.general.dao.PantallasDAO;
+import mx.com.gseguros.portal.general.model.ComponenteVO;
+import mx.com.gseguros.portal.general.util.GeneradorCampos;
 import mx.com.gseguros.utils.Utils;
 
+import org.apache.struts2.ServletActionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +21,9 @@ public class FlujoMesaControlManagerImpl implements FlujoMesaControlManager {
 	
 	@Autowired
 	private FlujoMesaControlDAO flujoMesaControlDAO;
+	
+	@Autowired
+	private PantallasDAO pantallasDAO;
 	
 	@Override
 	public Map<String,Item> workflow(
@@ -34,8 +42,16 @@ public class FlujoMesaControlManagerImpl implements FlujoMesaControlManager {
 		
 		try
 		{
-			paso = "Generando exception";
+			paso = "Recuperando componentes";
 			sb.append("\n").append(paso);
+			
+			GeneradorCampos gc=new GeneradorCampos(ServletActionContext.getServletContext().getServletContextName());
+			
+			List<ComponenteVO> ttipfluCmp = pantallasDAO.obtenerComponentes(null, null, null, null, null, cdsisrol, "FLUJOMC", "TTIPFLU", null);
+			gc.generaComponentes(ttipfluCmp, true, false, true, false, false, false);
+			
+			items.put("ttipfluFormItems" , gc.getItems());
+			
 		}
 		catch(Exception ex)
 		{
