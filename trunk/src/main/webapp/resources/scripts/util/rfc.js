8a9f26c46b17c0419abc00a83ffa946fe72f027e
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2016-01-05 17:23:17 +0000

    Se agrega la funcionalidad de generar RFC para persona Moral

---
+++ trunk/src/main/webapp/resources/scripts/util/rfc.js
@@ -0,0 +1,423 @@
+/**
+ * Valida un RFC a partir de sus datos de entrada
+ * @param {} tipoPersona Indica si es Fï¿½sica o Moral
+ * @param {} nombre Primer nombre
+ * @param {} nombre2 Segundo nombre
+ * @param {} apaterno 
+ * @param {} amaterno 
+ * @param {} fecha Fecha de nacimiento para persona fisica, fecha inicio para persona moral
+ * @param {} rfc RFC a validar
+ * @return true si es valido, false si es invalido
+ */
+function validaRFC(tipoPersona, nombre, nombre2, apaterno, amaterno, fecha, rfc) {
+    try {
+        // Persona fisica
+        if(tipoPersona == 'F') {
+            if(rfc.substr(0, 1) != apaterno.substr(0, 1)) {
+                return false;
+            }
+            if(rfc.substr(2, 1) != amaterno.substr(0, 1)) {
+                return false;
+            }
+            if(rfc.substr(3, 1) != nombre.substr(0, 1)) {
+                return false;
+            }
+            if(rfc.substr(4, 2) != fecha.substr(8, 2)) {
+                return false;
+            }
+            if(rfc.substr(6, 2) != fecha.substr(3, 2)) {
+                return false;
+            }
+            if(rfc.substr(8, 2) != fecha.substr(0, 2)) {
+                return false;
+            }
+        } else if(tipoPersona == 'M' || tipoPersona == 'S') { // Persona moral y regimen Simplificado
+            if(rfc.substr(0, 1) != nombre.substr(0, 1)) {
+                return false;
+            }
+            if(rfc.substr(1, 1) != nombre.substr(1, 1)) {
+                return false;
+            }
+            if(rfc.substr(2, 1) != nombre.substr(2, 1)) {
+                return false;
+            }
+            if(rfc.substr(3, 2) != fecha.substr(8, 2)) {
+                return false;
+            }
+            if(rfc.substr(5, 2) != fecha.substr(3, 2)) {
+                return false;
+            }
+            if(rfc.substr(7, 2) != fecha.substr(0, 2)) {
+                return false;
+            }
+        } else {
+            throw 'Error en la validacion, tipo de persona invalido: "' + tipoPersona + '"';
+        }
+        return true;
+    } catch(e) {
+        throw 'Error al validar RFC' + e.toString();
+    }
+}
+
+
+/**
+ * Genera un RFC de acuerdo a los datos solicitados
+ * @param {} tipoPersona
+ * @param {} nombre
+ * @param {} nombre2
+ * @param {} apaterno
+ * @param {} amaterno
+ * @param {} fecha
+ */
+function generaRFC(tipoPersona, nombre, nombre2, apaterno, amaterno, fecha) {
+    try {
+        if(tipoPersona == 'F') {
+            return apaterno.substr(0,2) + amaterno.substr(0,1) + nombre.substr(0, 1) +
+                fecha.substr(8, 2) + fecha.substr(3, 2) + fecha.substr(0, 2);
+        } else if(tipoPersona == 'M' || tipoPersona == 'S') { // Persona moral y regimen Simplificado
+            //return nombre.substr(0,3) + fecha.substr(8, 2) + fecha.substr(3, 2) + fecha.substr(0, 2);
+            return generaRFCPersonaMoral(nombre, fecha);
+        } else {
+            throw 'Error en la validacion, tipo de persona invalido';
+        }
+    } catch(e) {
+        throw 'Error al generar RFC' + e.toString();
+    }
+}
+
+
+function run(){
+   
+    var tipoPer = document.getElementById('tipoPersona').value;
+    var name = document.getElementById('nombre').value;
+    var name2 = document.getElementById('nombre2').value;
+    var paterno = document.getElementById('apaterno').value;
+    var materno = document.getElementById('amaterno').value;
+    var date = document.getElementById('fecha').value;
+    
+    //alert( cuentaPalabras(entrada) );
+    //alert('es vocal?' + esVocal('s')  );
+    
+    //document.getElementById('rfc').value = generaRFCPersonaMoral(name, date);
+    
+    document.getElementById('rfc').value = generaRFC(tipoPer, name, name2, paterno, materno, date);
+    
+    document.getElementById('numPalabras').value = cuentaPalabras(name);
+}
+
+
+/**
+ * Genera el RFC de una persona física
+ * @param {} nombre Nombre
+ * @param {} nombre2 Segundo nombre
+ * @param {} apaterno Apellido paterno
+ * @param {} amaterno Apellido materno
+ * @param {} fecha  Fecha de nacimiento en formato DD/MM/YYYY
+ */
+function generaRFCPersonaFisica(nombre, nombre2, apaterno, amaterno, fecha) {
+    
+    // TODO: Analizar si le pasamos el nombre 1 y 2 en una sola variable
+    
+    // REGLA 1
+    // Se integra la clave con los siguientes datos:
+    // 1.  La primera letra del apellido paterno y la siguiente primera vocal del mismo.
+	console.log('Parametros:', nombre, nombre2, apaterno, amaterno, fecha);
+    var tmp = apaterno.match(/\b(\w)/i)[0];
+    tmp += apaterno.match(/[aeiou]/i)[0];
+    console.log('Con apaterno:', tmp);
+    
+    // 2.  La primera letra del apellido materno.
+    tmp += amaterno.match(/\b(\w)/i)[0];
+    console.log('Con amaterno:', tmp);
+    
+    // 3.  La primera letra del nombre.
+    tmp += nombre.match(/\b(\w)/i)[0];
+    console.log('Con nombre:', tmp);
+    
+    
+    // REGLA 2
+    // A continuación se anotará la fecha de nacimiento del contribuyente, en el siguiente orden:
+    // Año: Se tomarán las dos últimas cifras, escribiéndolas con números arábigos.
+    // Mes: Se tomará el mes de nacimiento en su número de orden, en un año de calendario,                          escribiéndolo con números arábigos.
+    // Día: Se escribirá con números arábigos.
+    tmp += fecha.substr(8, 2) + fecha.substr(3, 2) + fecha.substr(0, 2);
+    console.log('Con fecha:', tmp);
+    
+    return tmp;
+}
+
+
+/**
+ * Genera el RFC de una persona moral
+ * @param {} nombre Nombre de la persona moral
+ * @param {} fecha  Fecha de constitución
+ */
+function generaRFCPersonaMoral(nombre, fecha) {
+    
+    // Se convierte a mayuscula y sin acentos:
+    nombre = nombre.toUpperCase();
+    nombre = nombre.replace(/\u00C1/gi, 'A')
+                   .replace(/\u00C9/gi, 'E')
+                   .replace(/\u00CD/gi, 'I')
+                   .replace(/\u00D3/gi, 'O')
+                   .replace(/\u00DA/gi,"U").replace(/\u00DC/,"U")
+                   .replace(/\u00D1/,"N");  
+    console.log('A mayusculas y sin acentos:', nombre);
+    
+    //
+    nombre = reemplazarCaracteresEspecialesPersonaMoral(nombre);
+    console.log('Reemplazar caracteres especiales por su descripcion:', nombre);
+    
+    // Se eliminan guiones, articulos, etc:
+    nombre = quitaEspaciosSignosYPuntos(nombre);
+    console.log('Sin articulos, puntos ni espacios:', nombre);
+    
+    // Regla 9.-
+    // Cuando en la denominación o razón social figuren artículos, preposiciones y conjunciones o contracciones 
+    // no se tomaran como elementos de integración de la clave:
+    nombre = quitaAbreviaturasPersonaMoral(nombre);
+    console.log('Sin abreviaturas persona Moral:', nombre);
+    
+    var numPalabras = cuentaPalabras(nombre);
+    console.log('numPalabras=', numPalabras);
+    
+    // Se obtiene la fecha de constitucion
+    
+    // Regla 1.-
+    
+    
+    // Regla 3:
+    // Cuando la letra inicial de cualquiera de las tres primeras palabras 
+    // de la denominación o razón social sea compuesta, 
+    // únicamente se anotará la inicial de esta.
+    // NO APLICA CODIGO
+    
+    //rfc = cuentaPalabras(nombre);
+    
+    if(numPalabras == 1) {
+        nombre = obtienePalabra(nombre, 1).substr(0, 3);
+    } else if(numPalabras == 2) {
+        // REGLA 6ª.
+        // Si la denominación o razón social se comprende de dos elementos, 
+        // para efectos de la conformación de la clave, se tomará la letra inicial de la primera palabra 
+        // y las dos primeras letras de la segunda
+        nombre = obtienePalabra(nombre, 1).substr(0, 1) + obtienePalabra(nombre, 2).substr(0, 2); 
+        
+    } else if(numPalabras >= 3) {
+        
+        nombre = obtienePalabra(nombre, 1).substr(0, 1)
+                + obtienePalabra(nombre, 2).substr(0, 1)
+                + obtienePalabra(nombre, 3).substr(0, 1);
+        
+    } else {
+        //throw 'No existen palabras para generar el RFC: "' + nombre + '"';
+    }
+    
+    // Regla 8:
+    // Cuando la denominación o razón social se componga de un solo elemento y sus letras no completen las tres requeridas, 
+    // para efectos de conformación de la clave, se tomaran las empleadas por el contribuyente 
+    // y las restantes se suplirán con una “X”:
+    nombre = padding_right(nombre, 'X', 3);
+    
+    
+    // Regla 2:
+    // Se agrega la fecha de constitucion:
+    nombre += fecha.substr(8, 2) + fecha.substr(3, 2) + fecha.substr(0, 2);
+    
+    return nombre;
+}
+
+
+/**
+ * Cuenta el numero de palabras en un texto
+ * @param {} txt Texto de entrada
+ * @return Numero de palabras encontradas
+ */
+function cuentaPalabras(txt) {
+	txt = txt.replace(/[\.]{1,}/gi," "); // reemplazamos los puntos por espacios
+    txt = txt.replace(/(^\s*)|(\s*$)/gi,"");
+    txt = txt.replace(/[ ]{2,}/gi," ");
+    txt = txt.replace(/\n /,"\n");
+    
+    console.log('txt=="', txt, '"');
+    
+    if(txt.length > 0) {
+    	return txt.split(" ").length;
+    } else {
+    	return 0;
+    }
+}
+
+/**
+ * 
+ * @param {} txt
+ * @return {}
+ */
+function obtienePalabra(txt, pos) {
+    txt = txt.replace(/[\.]{1,}/gi," "); // reemplazamos los puntos por espacios
+    txt = txt.replace(/(^\s*)|(\s*$)/gi,"");
+    txt = txt.replace(/[ ]{2,}/gi," ");
+    txt = txt.replace(/\n /,"\n");
+    
+    return txt.split(" ")[pos-1];
+}
+
+/**
+ * 
+ * @param {} palabra
+ * @return {}
+ */
+function quitaEspaciosSignosYPuntos(texto) {
+    return texto.replace(/[,]{1,}/gi," ") // reemplazamos las comas por espacios
+                .replace(/[\.]{1,}/gi," ") // reemplazamos los puntos por espacios
+                .replace(/(^\s*)|(\s*$)/gi,"")
+                .replace(/[ ]{2,}/gi," ")
+                .replace(/\n /,"\n");
+    return txt;
+}
+
+
+function quitaAbreviaturasPersonaMoral(texto) {
+	
+	return texto.replace('-', '')
+            	//.replace('A C ', '')
+	            .replace(/\b(A C)\b/gi, '')
+	            //.replace('A  EN P ', '')
+	            .replace(/\b(A  EN P)\b/gi, '')
+                //.replace('S DE RL', '').replace('S DE R L', '').replace('S  DE R L', '')
+                .replace(/\b(S DE RL)\b/gi, '').replace(/\b(S DE R L)\b/gi, '').replace(/\b(S  DE R L)\b/gi, '')
+                //.replace('COMPAÑÍA ', '').replace('COMPAÑIA ', '').replace('COMPANIA ', '')
+                .replace(/\b(COMPAÑÍA)\b/gi, '').replace(/\b(COMPAÑIA)\b/gi, '').replace(/\b(COMPANIA)\b/gi, '')
+                //.replace('CIA ', '').replace('CÍA ', '')
+                .replace(/\b(CIA)\b/gi, '').replace(/\b(CÍA)\b/gi, '')
+                //.replace('SOCIEDAD ', '')
+                .replace(/\b(SOCIEDAD)\b/gi, '')
+                //.replace('COOPERATIVA ', '')
+                .replace(/\b(COOPERATIVA)\b/gi, '')
+                //.replace('S EN C POR A ', '').replace('S  EN C POR A ', '')
+                .replace(/\b(S EN C POR A)\b/gi, '').replace(/\b(S  EN C POR A)\b/gi, '')
+                //.replace('S EN NC ', '').replace('S EN N C  ', '')
+                .replace(/\b(S EN NC)\b/gi, '').replace(/\b(S EN N C)\b/gi, '')
+                //.replace('S A  DE C V ', '').replace('S A', '')
+                .replace(/\b(SA DE CV)\b/gi, '').replace(/\b(S A DE C V)\b/gi, '').replace(/\b(S A  DE C V)\b/gi, '').replace(/\b(S A)\b/gi, '')
+                //.replace('PARA ', '')
+                .replace(/\b(PARA)\b/gi, '')
+                //.replace('POR ', '')
+                .replace(/\b(POR)\b/gi, '')
+                //.replace('AL ', '')
+                .replace(/\b(AL)\b/gi, '')
+                //.replace('S C ', '').replace('S C S ', '')
+                .replace(/\b(S C)\b/gi, '').replace(/\b(S C S)\b/gi, '')
+                //.replace('SCL ', '').replace('S C L ', '')
+                .replace(/\b(SCL)\b/gi, '').replace(/\b(S C L)\b/gi, '')
+                //.replace('SNC ', '').replace('S N C ', '')
+                .replace(/\b(SNC)\b/gi, '').replace(/\b(S N C)\b/gi, '')
+                //.replace('OF ', '')
+                .replace(/\b(OF)\b/gi, '')
+                //.replace('COMPANY ', '')
+                .replace(/\b(COMPANY)\b/gi, '')
+                //.replace('MC ', '')
+                .replace(/\b(MC)\b/gi, '')
+                //.replace('VON ', '')
+                .replace(/\b(VON)\b/gi, '')
+                //.replace('MI ', '')
+                .replace(/\b(MI)\b/gi, '')
+                //.replace('SRL CV ', '')
+                .replace(/\b(SRL CV)\b/gi, '')
+                //.replace('SA MI ', '')
+                .replace(/\b(SA MI)\b/gi, '')
+                //.replace('SRL CV MI ', '')
+                .replace(/\b(SRL CV MI)\b/gi, '')
+                // Articulos:
+                //.replace(' DE ', '')
+                .replace(/\b(DE)\b/gi, '')
+                //.replace(' DEL ', '')
+                .replace(/\b(DEL)\b/gi, '')
+                //.replace(' EL ', '')
+                .replace(/\b(EL)\b/gi, '')
+                //.replace(' LA ', '')
+                .replace(/\b(LA)\b/gi, '')
+                //.replace(' LOS ', '')
+                .replace(/\b(LOS)\b/gi, '')
+                //.replace(' LAS ', '')
+                .replace(/\b(LAS)\b/gi, '')
+                //.replace(' E ', '') // TODO: Crear una funcion que busque coincidencias por palabra completa
+                .replace(/\b(E)\b/gi, '')
+                //.replace(' Y ', '')
+                .replace(/\b(Y)\b/gi, '');
+                //.replace(/(^\s*)|(\s*$)/gi,"")
+                
+}
+
+function reemplazarCaracteresEspecialesPersonaMoral(txt) {
+	
+	// REGLA 12
+    // Cuando aparezcan formando parte de la denominación o razón social los caracteres especiales, 
+	// éstos deben de excluirse para el cálculo del homónimo y del dígito verificador. 
+	// Los caracteres se interpretarán, sí y sólo si, están en forma individual dentro del texto de la denominación o razón social.
+	txt = txt.replace(/@(?=\s|$)/gi, 'ARROBA')
+	         .replace(/\u0027(?=\s|$)/gi, 'APOSTROFE').replace(/\u2018(?=\s|$)/gi, 'APOSTROFE').replace(/\u2019(?=\s|$)/gi, 'APOSTROFE')
+	         .replace(/\%(?=\s|$)/gi, 'PORCIENTO')
+	         .replace(/#(?=\s|$)/gi, 'NUMERO')
+	         .replace(/!(?=\s|$)/gi, 'ADMIRACION')
+	         //.replace(/\.(?=\s|$)/gi, 'PUNTO')// TODO: buscar expresion regular que lo arregle
+	         .replace(/\$(?=\s|$)/gi, 'PESOS')
+	         .replace(/\x22(?=\s|$)/gi, 'COMILLAS').replace(/\u201C(?=\s|$)/gi, 'COMILLAS').replace(/\u201D(?=\s|$)/gi, 'COMILLAS')
+	         .replace(/-(?=\s|$)/gi, 'GUION')
+	         .replace(/\/(?=\s|$)/gi, 'DIAGONAL')
+	         .replace(/\+(?=\s|$)/gi, 'SUMA')
+	         .replace(/\((?=\s|$)/gi, 'ABRE PARENTESIS')
+	         .replace(/\)(?=\s|$)/gi, 'CIERRA PARENTESIS');
+	// Ahora se reemplazan los caracteres especiales que no vienen en forma individual:
+	txt = txt.replace(/@/gi, '')
+	         .replace(/\u0027/gi, '').replace(/\u2018/gi, '').replace(/\u2019/gi, '')
+             .replace(/\%/gi, '')
+             .replace(/#/gi, '')
+             .replace(/!/gi, '')
+             //.replace(/\.(?=\s|$)/gi, '')// TODO: buscar expresion regular que lo arregle
+             .replace(/\$/gi, '')
+             .replace(/\x22/gi, '').replace(/\u201C/gi, '').replace(/\u201D/gi, '')
+             .replace(/-/gi, '')
+             .replace(/\//gi, '')
+             .replace(/\+/gi, '')
+             .replace(/\(/gi, '')
+             .replace(/\)/gi, '');
+    return txt;
+}
+
+
+/**
+ * Indica si la letra es vocal
+ * @param {} letra
+ * @return {Boolean} true si es vocal, false si no lo es
+ */
+function esVocal(letra) {
+    if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O'
+            || letra == 'U' || letra == 'a' || letra == 'e' || letra == 'i'
+            || letra == 'o' || letra == 'u')
+        return true;
+    else
+        return false;
+}
+
+
+
+/**
+ * right padding s with c to a total of n chars
+ * Fuente: http://eureka.ykyuen.info/2011/08/23/javascript-leftright-pad-a-string/
+ * @param {} s
+ * @param {} c
+ * @param {} n
+ * @return {}
+ */
+function padding_right(s, c, n) {
+  if (! s || ! c || s.length >= n) {
+    return s;
+  }
+  var max = (n - s.length)/c.length;
+  for (var i = 0; i < max; i++) {
+    s += c;
+  }
+  return s;
+}
+
