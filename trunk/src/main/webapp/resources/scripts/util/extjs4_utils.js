3d79c198525405b0cfc78630ee687e6a2f66b3fa
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2016-02-03 01:52:59 +0000

    2016-02-02a Paginacion para asegurados y extraprimas en MSC

--- trunk/src/main/webapp/resources/scripts/util/extjs4_utils.js
+++ trunk/src/main/webapp/resources/scripts/util/extjs4_utils.js
@@ -48,7 +48,9 @@ function showMessage(title, msg, buttons, icon){
  * @param {Ext.grid.Panel} _grid (Optional)
  * @param {Number} _timeout (Optional)
  */
-function cargaStorePaginadoLocal(_store, _url, _root, _params, _callback, _grid, _timeout) {
+function cargaStorePaginadoLocal(_store, _url, _root, _params, _callback, _grid, _timeout, filterFn) {
+	
+	debug('cargaStorePaginadoLocal filterFn:',filterFn);
 	
 	var timeOut = null;
 	if(_timeout) {
@@ -61,50 +63,96 @@ function cargaStorePaginadoLocal(_store, _url, _root, _params, _callback, _grid,
 			_grid.down('pagingtoolbar').moveFirst();
 		}
 	}
-    Ext.Ajax.request(
-    {
-        url       : _url,
-        params    : _params,
-        callback  : _callback,
-        timeout   : timeOut != null ? timeOut : Ext.Ajax.timeout, 
-        success   : function(response)
-        {
-        	if(_grid){
-        		_grid.setLoading(false);
-        	}
-        	_store.removeAll();
-            var jsonResponse = Ext.decode(response.responseText);
-            _store.setProxy({
-                type         : 'memory',
-                enablePaging : true,
-                reader       : 'json',
-                data         : jsonResponse[_root]
-            });
-            _store.load();
-        },
-        failure   : function()
-        {
-        	if(_grid){
-        		_grid.setLoading(false);
-        	}
-        	_store.removeAll();
-            Ext.Msg.show({
-                title   : 'Error',
-                icon    : Ext.Msg.ERROR,
-                //msg     : 'Error cargando los datos de ' + _url,
-                msg     : 'Error al obener los datos, intente m\u00E1s tarde',
-                buttons : Ext.Msg.OK
-            });
-        }
-    });
+	
+	if(Ext.isEmpty(filterFn))
+	{
+	    Ext.Ajax.request(
+	    {
+	        url       : _url,
+	        params    : _params,
+	        callback  : _callback,
+	        timeout   : timeOut != null ? timeOut : Ext.Ajax.timeout, 
+	        success   : function(response)
+	        {
+	        	if(_grid){
+	        		_grid.setLoading(false);
+	        	}
+	        	_store.removeAll();
+	            var jsonResponse = Ext.decode(response.responseText);
+	            var records = [];
+	            var modelo = _store.getProxy().getModel();
+	            debug('#modelo:',modelo);
+	            for(var i in jsonResponse[_root])
+	            {
+	                records.push(modelo.create(jsonResponse[_root][i]));
+	            }
+	            debug('#records:',records);
+	            _store.datos = records;
+	            _store.setProxy({
+	                type         : 'memory',
+	                enablePaging : true,
+	                reader       : 'json',
+	                data         : records
+	            });
+	            _store.load();
+	            _store.commitChanges();
+	        },
+	        failure   : function()
+	        {
+	        	if(_grid){
+	        		_grid.setLoading(false);
+	        	}
+	        	_store.removeAll();
+	            Ext.Msg.show({
+	                title   : 'Error',
+	                icon    : Ext.Msg.ERROR,
+	                //msg     : 'Error cargando los datos de ' + _url,
+	                msg     : 'Error al obener los datos, intente m\u00E1s tarde',
+	                buttons : Ext.Msg.OK
+	            });
+	        }
+	    });
+	}
+	else
+	{
+	    setTimeout(function()
+	    {
+	        /* jtezva
+	        En esta parte filtramos los records, los records ya estÃ¡n cargados en store.datos, solo los filtramos con
+	        la funcion recibida, la funcion recibe el records y regresa true/false, con false se quita, con true se incluye
+	        */
+	        _store.removeAll();
+	        var records   = _store.datos;
+	        var incluidos = [];
+	        for(var i in records)
+	        {
+	            var rec = records[i];
+     	        if(filterFn(rec))
+	            {
+	                incluidos.push(rec);
+	            }
+	        }
+	        _store.setProxy({
+	            type          : 'memory'
+	            ,enablePaging : true
+	            ,reader       : 'json'
+	            ,data         : incluidos
+	        });
+	        _store.load();
+	        if(_grid)
+	        {
+	            _grid.setLoading(false);
+	        }
+	    },300);
+	}
 }
 
 
 /**
- * Maneja los errores en un submit de un formulario e invoca la función callback enviada como parametro
+ * Maneja los errores en un submit de un formulario e invoca la funciï¿½n callback enviada como parametro
  *
- * @param {Ext.form.Basic} form Formulario que solicitó la acción
- * @param {Ext.form.action.Action} action El objeto Action que ejecutó la operación
+ * @param {Ext.form.Basic} form Formulario que solicitï¿½ la acciï¿½n
+ * @param {Ext.form.action.Action} action El objeto Action que ejecutï¿½ la operaciï¿½n
  * @param {Function} clbkFn Funcion callback a ejecutar
  */
 function manejaErrorSubmit(form, action, clbkFn) {
