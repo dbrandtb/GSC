bc6b6b44605bd353cdfbb4d50b78c0e69a4eb41b
rbautista <rbautista@68539698-d818-11e6-ad83-4fbd610fe048>
2016-04-11 21:38:37 +0000

    Se aumenta el valor de timeout.

--- trunk/src/main/webapp/resources/scripts/consultaPolizas/consultasPolizas.js
+++ trunk/src/main/webapp/resources/scripts/consultaPolizas/consultasPolizas.js
@@ -6,7 +6,7 @@ var windowCoberturas;
 Ext.onReady(function() {
 	
     // Se aumenta el timeout para todas las peticiones:
-    Ext.Ajax.timeout = 1*1000*60;
+    Ext.Ajax.timeout = 1000*60*3; // 3 minutos
     Ext.override(Ext.form.Basic, { timeout: Ext.Ajax.timeout / 1000 });
     Ext.override(Ext.data.proxy.Server, { timeout: Ext.Ajax.timeout });
     Ext.override(Ext.data.Connection, { timeout: Ext.Ajax.timeout });
