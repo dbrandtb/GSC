601b5c84dd1940d5fee3fae9762389b623913d6c
jtezva <jtezva@68539698-d818-11e6-ad83-4fbd610fe048>
2016-08-03 16:59:58 +0000

    2016-08-03b Se emparejan mensajes despues de carga completa para ambas pantallas

--- trunk/src/main/webapp/js/proceso/emision/funcionesCotizacionGrupo.js
+++ trunk/src/main/webapp/js/proceso/emision/funcionesCotizacionGrupo.js
@@ -279,8 +279,8 @@ function _p21_subirArchivoCompleto(button,nombreCensoParaConfirmar)
                         {
                         
                             //var callback = function() { _p21_turnar(19,'Observaciones de la carga',false); };
-                            var callback = function() { mensajeCorrecto('Aviso','Se ha turnado el tr\u00e1mite a mesa de control ' +
-                            							'para procesar el censo. Una vez terminado podra encontrar su tr\u00e1mite ' +
+                            var callback = function() { mensajeCorrecto('Aviso','Se ha turnado el tr\u00e1mite a mesa de control en estatus ' +
+                            							'"En Tarifa" para procesar el censo. Una vez terminado podra encontrar su tr\u00e1mite ' +
                             							'en estatus "Carga completa".',function(){ _p21_mesacontrol(); } 
                             							);
                             						  };
