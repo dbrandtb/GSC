8be6e3afa5d4189b90d93b695b8d112cb4948fa0
Dafna Brandt <31972858+dbrandtb@users.noreply.github.com>
2016-10-28 14:34:36 +0000

    Modificación de toString()

--- src/main/java/mx/com/gseguros/portal/consultas/model/CopagoVO.java
+++ src/main/java/mx/com/gseguros/portal/consultas/model/CopagoVO.java
@@ -99,6 +99,6 @@ public class CopagoVO implements Serializable {
 
 	@Override
 	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
 	}
 }
