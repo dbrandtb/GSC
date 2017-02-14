9f234bafa21f5586d31c07b148b8672597456843
dafna.brandt <dafna.brandt@68539698-d818-11e6-ad83-4fbd610fe048>
2017-02-14 20:16:36 +0000

    version desplegada en QA el 2017/02/14. cubre requerimiento: 	REQ003

---
+++ tags/gseguros_clientes-b0.9.4/src/main/java/mx/com/gseguros/portal/general/util/TipoSituacion.java
@@ -0,0 +1,36 @@
+package mx.com.gseguros.portal.general.util;
+
+public enum TipoSituacion {
+	
+	AUTOS_FRONTERIZOS         ("AF"),
+	AUTOS_PICK_UP             ("PU"),
+	AUTOS_RESIDENTES          ("AR"),
+	CAMIONES_CARGA            ("CR"),
+	PICK_UP_CARGA             ("PC"),
+	PICK_UP_PARTICULAR        ("PP"),
+	MULTISALUD                ("MS"),
+	MULTISALUD_COLECTIVO      ("MSC"),
+	SALUD_NOMINA              ("SN"),
+	SALUD_VITAL               ("SL"),
+	SERVICIO_PUBLICO_AUTO     ("AT"),
+	SERVICIO_PUBLICO_MICRO    ("MC"),
+	RECUPERA_INDIVIDUAL       ("RI"),
+	RECUPERA_COLECTIVO        ("RC"),
+	GASTOS_MEDICOS_INDIVIDUAL ("GMI"),
+	TRADICIONALES_MEGASALUD   ("TMS"),
+	TRADICIONALES_SUSALUD     ("TSS"),
+	MOTOS                     ("MO"),
+	TRACTOCAMIONES_ARMADOS    ("TC")
+	;
+
+	private String cdtipsit;
+
+	private TipoSituacion(String cdtipsit) {
+		this.cdtipsit = cdtipsit;
+	}
+
+	public String getCdtipsit() {
+		return cdtipsit;
+	}
+	
+}
