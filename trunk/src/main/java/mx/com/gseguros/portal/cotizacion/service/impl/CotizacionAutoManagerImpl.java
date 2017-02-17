416cd98c3f55efc801a886dfcf5abaffd45f8a9c
jaime.villar <jaime.villar@68539698-d818-11e6-ad83-4fbd610fe048>
2017-02-17 00:43:17 +0000

    Se cambia acentuacion en el msn que se envia por correo a HTML Code

--- trunk/src/main/java/mx/com/gseguros/portal/cotizacion/service/impl/CotizacionAutoManagerImpl.java
+++ trunk/src/main/java/mx/com/gseguros/portal/cotizacion/service/impl/CotizacionAutoManagerImpl.java
@@ -2589,7 +2589,7 @@ public class CotizacionAutoManagerImpl implements CotizacionAutoManager
   	            	logger.debug("Amis Y Modelo con Irregularidades en coberturas: "+planValido);
   	            	String mensajeAPantalla = "Por el momento no es posible cotizar para esta unidad, el paquete de cobertura Prestigio, Amplio  y Limitado, le pedimos por favor ponerse en contacto con su ejecutivo de ventas.";
   	            	resp.getSmap().put("msnPantalla" , mensajeAPantalla);
-  	            	String mensajeACorreo= "Se le notifica que no ha sido posible cotizar la solicitud "+nmpoliza+" del producto de Autom�viles en el paquete de cobertura Prestigio, Amplio  y Limitado:\n" + 
+  	            	String mensajeACorreo= "Se le notifica que no ha sido posible cotizar la solicitud "+nmpoliza+" del producto de Autom&oacute;viles en el paquete de cobertura Prestigio, Amplio  y Limitado:\n" + 
   	            			planValido;
   	            	
   	            	String [] listamails = cotizacionDAO.obtenerCorreosReportarIncidenciasPorTipoSituacion(cdramo);
