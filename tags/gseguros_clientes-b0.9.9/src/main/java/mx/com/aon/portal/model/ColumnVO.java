9da8597a5947568412ab547c41e15beb4f0fa215
dafna.brandt <dafna.brandt@68539698-d818-11e6-ad83-4fbd610fe048>
2017-02-20 22:15:51 +0000

    desplegado el 20170220 14:00 b 0.9.9. correccion INC Jaime CotizacionAutoAction

---
+++ tags/gseguros_clientes-b0.9.9/src/main/java/mx/com/aon/portal/model/ColumnVO.java
@@ -0,0 +1,19 @@
+package mx.com.aon.portal.model;
+
+public class ColumnVO {
+	private String name;
+	private String value;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
