878de3a7d18d6701c66c660c5c4e155919ad2577
dafna.brandt <dafna.brandt@68539698-d818-11e6-ad83-4fbd610fe048>
2017-03-22 01:47:27 +0000

    No funciono la integracion de cesar y se realizó nueva version con integracion parcial

--- tags/gseguros_clientes_preprod-b0.9.10.8/src/main/java/mx/com/gseguros/portal/general/service/EjecutarComandoSshService.java
+++
@@ -1,7 +0,0 @@
-package mx.com.gseguros.portal.general.service;
-
-import java.util.List;
-
-public interface EjecutarComandoSshService {
-	public String ejectutarCmd(String server,String usuario,String pass, List<String> cmd) throws Exception;
-}
