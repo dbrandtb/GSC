a9766cbe1de217223b7b4e2b401b0f4183827f56
hector.lopez <hector.lopez@68539698-d818-11e6-ad83-4fbd610fe048>
2017-05-23 19:46:14 +0000

    QA | gseguros_clientes v 0.9.81 20170522_INC0006_JAVA_GSEGUROS_CLIENTES_JLLV_VALIDACION_RENOVACION_FACULTA 20170522_REQ73_JAVA_GSEGUROS_CLIENTES_JAGC_VALIDACION_AUTORIZACION_SERVICIO_X_COBERTURAS

---
+++ tags/gseguros_clientes-b0.9.81/src/main/java/mx/com/gseguros/confpantallas/action/BuscaControl.java
@@ -0,0 +1,79 @@
+package mx.com.gseguros.confpantallas.action;
+
+import mx.com.gseguros.confpantallas.delegate.AdminBuscaControlDelegate;
+
+/**
+ * Does some thing in old style.
+ *
+ * @deprecated use {@link BuscaControlAction} instead.  
+ */
+@Deprecated
+public class BuscaControl {
+	private String tarea;
+	private String regreso;
+	private String strRamo;
+	private String campo;
+	private String tabla;
+	
+	public String execute() {
+		
+		AdminBuscaControlDelegate admBusca = new AdminBuscaControlDelegate();
+		Integer sep = getTabla().indexOf("_");
+		String tablaO = "";String query = "";
+		if(sep > -1){
+			tablaO = getTabla().substring(sep+1);
+		}
+		if(getTarea().equals("getDatosCtl")){
+			int c = getStrRamo().indexOf("_");
+			String r = getStrRamo().substring(c+1);
+			if(tablaO.equals("TATRIPOL")){
+				query = "DatosControlTatripol";
+			}else if(tablaO.equals("TATRISIT")){
+				query = "DatosControlTatrisit";
+			}
+			setRegreso(admBusca.gatDataControl(r, getCampo(), query));
+		}
+		
+		return "SUCCESS";
+	}
+
+	public String getTarea() {
+		return tarea;
+	}
+
+	public void setTarea(String tarea) {
+		this.tarea = tarea;
+	}
+
+	public String getRegreso() {
+		return regreso;
+	}
+
+	public void setRegreso(String regreso) {
+		this.regreso = regreso;
+	}
+
+	public String getStrRamo() {
+		return strRamo;
+	}
+
+	public void setStrRamo(String strRamo) {
+		this.strRamo = strRamo;
+	}
+
+	public String getCampo() {
+		return campo;
+	}
+
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+
+	public String getTabla() {
+		return tabla;
+	}
+
+	public void setTabla(String tabla) {
+		this.tabla = tabla;
+	}
+}
