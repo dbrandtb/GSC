b052c6474f2e6d4bae71f54ee673bd7d87ce160f
durodriguezr <durodriguezr@68539698-d818-11e6-ad83-4fbd610fe048>
2017-08-28 18:14:57 +0000

    Renovaciones con endosos B. Implementación de emisión directa de endosos B: Adaptaciones para efectos de RC; Beneficiario Preferente.

--- trunk/src/main/java/mx/com/gseguros/portal/cotizacion/controller/ComplementariosAction.java
+++ trunk/src/main/java/mx/com/gseguros/portal/cotizacion/controller/ComplementariosAction.java
@@ -4812,8 +4812,8 @@ public class ComplementariosAction extends PrincipalCoreAction
 			
 			if(StringUtils.isNotBlank(infoPoliza.get("RENUNIEXT")) && StringUtils.isNotBlank(infoPoliza.get("RENRAMO")) && StringUtils.isNotBlank(infoPoliza.get("RENPOLIEX")) && (infoPoliza.get("RENRAMO").contains("711") || infoPoliza.get("RENRAMO").contains("721")))
 			{
-				//int[] motend = {44,75};
-				int[] motend = {75}; //SOLO ENDOSO LIBRE PARA PRODUCCIÓN
+				int[] motend = {44,45,75,76};
+
 				slist1 = new ArrayList<Map<String, String>>();
 				
 				for(int i = 0; i < motend.length; i++){
