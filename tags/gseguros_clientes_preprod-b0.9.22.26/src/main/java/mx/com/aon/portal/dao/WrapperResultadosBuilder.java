9b881190e4fa3feedd2dfaa7ad4c75bd1a3d353f
dafna.brandt <dafna.brandt@68539698-d818-11e6-ad83-4fbd610fe048>
2017-09-08 01:33:21 +0000

    QA | GSEGUROS_CLIENTES_PREPROD b 0.9.22.26 20170830_REQ0073_GSEGUROS_CLIENTES_SALUD_EGS_RESTRIC_PAGO_DIRECTO

---
+++ tags/gseguros_clientes_preprod-b0.9.22.26/src/main/java/mx/com/aon/portal/dao/WrapperResultadosBuilder.java
@@ -0,0 +1,9 @@
+package mx.com.aon.portal.dao;
+
+import java.util.Map;
+
+import mx.com.aon.portal.util.WrapperResultados;
+@Deprecated
+public interface WrapperResultadosBuilder {
+    public WrapperResultados build(Map map) throws Exception;
+}
---
+++ tags/gseguros_clientes_preprod-b0.9.22.26/src/main/java/mx/com/gseguros/portal/general/util/Rango.java
@@ -0,0 +1,22 @@
+package mx.com.gseguros.portal.general.util;
+
+public enum Rango {
+	
+	ANIOS("1"),
+	DIAS("2"),
+	PESOS("3"),
+	LONGITUD("4"),
+	MESES("5"),
+	ANIOSASEG("6");
+
+	private String clave;
+
+	private Rango(String clave) {
+		this.clave = clave;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+	
+}
